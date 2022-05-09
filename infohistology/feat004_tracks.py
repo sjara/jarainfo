25aec0eabc307f72a645e9ac5e3034754e2ea5bf
JaraLab User <jlmohn@ucdavis.edu>
2022-05-09 12:47:22 -0700

    fixed feat004_tracks shank labels to 1 (was 0)

--- infohistology/feat004_tracks.py
+++ infohistology/feat004_tracks.py
@@ -1,10 +1,10 @@
 subject = 'feat004'
 tracks = [
     {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_C3_right',
-     'recordingTrack':'anteromedialDiD', 'shank':0, 'atlasZ':201},
+     'recordingTrack':'anteromedialDiD', 'shank':1, 'atlasZ':201},
 
     {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C2_left',
-     'recordingTrack':'caudomedialDiD', 'shank':0, 'atlasZ':194},
+     'recordingTrack':'caudomedialDiD', 'shank':1, 'atlasZ':194},
 
     {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B4_left',
-     'recordingTrack':'caudolateralDiI', 'shank':0, 'atlasZ':183}]
+     'recordingTrack':'caudolateralDiI', 'shank':1, 'atlasZ':183}]
