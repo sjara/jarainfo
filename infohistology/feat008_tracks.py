d6763ca685848b0453b16d81583b3c343748f60c
Jenny Mohn <jlmohn@ucdavis.edu>
2022-05-10 18:18:33 -0700

    made feat008 tracks file

---
+++ infohistology/feat008_tracks.py
@@ -0,0 +1,11 @@
+subject = 'feat008'
+tracks = [
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B6',
+     'recordingTrack':'anteriorlateral_DiI', 'shank':1, 'atlasZ':203},#tip: p1_a6
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B6',
+     'recordingTrack':'anteriormedial_DiD', 'shank':1, 'atlasZ':203}, #tip: p1_A6
+
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B4',
+     'recordingTrack':'caudomedial_DiI', 'shank':1, 'atlasZ':194}] #tip: p1_A4
