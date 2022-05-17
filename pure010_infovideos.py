931724b9e43023967b15720b27eb007474cf382a
Manuel <manuelo@uoregon.edu>
2022-05-17 08:19:58 -0700

    infovideo file for pure010

--- pure010_infovideos.py
+++ pure010_infovideos.py
@@ -5,161 +5,180 @@ Information about videos.
 from jaratoolbox import videoinfo
 
         
-subject = 'pure003'
+subject = 'pure010'
 videos = videoinfo.Videos(subject)
 cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 
 videos.add_session('2022-02-15', '2chords',
-                   'pure010_20220215_2Sounds_113_2Sconfig9.mp4',
+                   'pure010_20220215_2Sounds_113_2Sconfig9.mkv',
                    'pure010_detectiongonogo_20220215_2Sounds_113_2Sconfig9.h5',
                    cameraParams) 
                    
 videos.add_session('2022-02-15', '2chords',
-                   'pure010_20220215_2Sounds_114_2Sconfig9.mp4',
+                   'pure010_20220215_2Sounds_114_2Sconfig9.mkv',
                    'pure010_detectiongonogo_20220215_2Sounds_114_2Sconfig9.h5',
                    cameraParams)
                    
 videos.add_session('2022-02-15', '2chords',
-                   'pure010_20220215_2Sounds_115_2Sconfig9.mp4',
+                   'pure010_20220215_2Sounds_115_2Sconfig9.mkv',
                    'pure010_detectiongonogo_20220215_2Sounds_115_2Sconfig9.h5',
                    cameraParams)
 
 videos.add_session('2022-02-15', '2chords',
-                   'pure010_20220215_2Sounds_115_2Sconfig9.mp4',
+                   'pure010_20220215_2Sounds_115_2Sconfig9.mkv',
                    'pure010_detectiongonogo_20220215_2Sounds_115_2Sconfig9.h5',
                    cameraParams)
 
 videos.add_session('2022-02-16', 'chord',
-                   'pure010_20220216_syncSound_76_config20.mp4',
+                   'pure010_20220216_syncSound_76_config20.mkv',
                    'pure010_detectsound_20220216_syncSound_76_config20.h5',
                    cameraParams)                                     
 
 videos.add_session('2022-02-16', 'chord',
-                   'pure010_20220216_syncSound_77_config21.mp4',
+                   'pure010_20220216_syncSound_77_config21.mkv',
                    'pure010_detectsound_20220216_syncSound_77_config21.h5',
                    cameraParams)                    
 
 videos.add_session('2022-02-16', 'chord',
-                   'pure010_20220216_syncSound_78_config22.mp4',
+                   'pure010_20220216_syncSound_78_config22.mkv',
                    'pure010_detectsound_20220216_syncSound_78_config22.h5',
                    cameraParams)
 
 videos.add_session('2022-02-17', '2chords',
-                   'pure010_20220217_2Sounds_128_2Sconfig10.mp4',
+                   'pure010_20220217_2Sounds_128_2Sconfig10.mkv',
                    'pure010_detectiongonogo_20220217_2Sounds_128_2Sconfig10.h5',
                    cameraParams)
 
 videos.add_session('2022-02-17', '2chords',
-                   'pure010_20220217_2Sounds_129_2Sconfig10.mp4',
+                   'pure010_20220217_2Sounds_129_2Sconfig10.mkv',
                    'pure010_detectiongonogo_20220217_2Sounds_129_2Sconfig10.h5',
                    cameraParams)
 
 videos.add_session('2022-02-17', '2chords',
-                   'pure010_20220217_2Sounds_130_2Sconfig10.mp4',
+                   'pure010_20220217_2Sounds_130_2Sconfig10.mkv',
                    'pure010_detectiongonogo_20220217_2Sounds_130_2Sconfig10.h5',
                    cameraParams)
 
 videos.add_session('2022-02-22', '2chords',
-                   'pure010_20220222_2Sounds_143_2Sconfig11.mp4',
+                   'pure010_20220222_2Sounds_143_2Sconfig11.mkv',
                    'pure010_detectiongonogo_20220222_2Sounds_143_2Sconfig11.h5',
                    cameraParams)
 
 videos.add_session('2022-02-22', '2chords',
-                   'pure010_20220222_2Sounds_144_2Sconfig11.mp4',
+                   'pure010_20220222_2Sounds_144_2Sconfig11.mkv',
                    'pure010_detectiongonogo_20220222_2Sounds_144_2Sconfig11.h5',
                    cameraParams)
 
 videos.add_session('2022-02-22', '2chords',
-                   'pure010_20220222_2Sounds_145_2Sconfig11.mp4',
+                   'pure010_20220222_2Sounds_145_2Sconfig11.mkv',
                    'pure010_detectiongonogo_20220222_2Sounds_145_2Sconfig11.h5',
                    cameraParams)
 
 videos.add_session('2022-03-22', '2chords',
-                   'pure010_20220322_2Sounds_165_2Sconfig12.mp4',
+                   'pure010_20220322_2Sounds_165_2Sconfig12.mkv',
                    'pure010_detectiongonogo_20220322_2Sounds_165_2Sconfig12.h5',
                    cameraParams)
 
 videos.add_session('2022-03-23', '2chords',
-                   'pure010_20220323_2Sounds_166_2Sconfig12.mp4',
+                   'pure010_20220323_2Sounds_166_2Sconfig12.mkv',
                    'pure010_detectiongonogo_20220323_2Sounds_166_2Sconfig12.h5',
                    cameraParams)
 
 videos.add_session('2022-03-23', '2chords',
-                   'pure010_20220323_2Sounds_167_2Sconfig12.mp4',
+                   'pure010_20220323_2Sounds_167_2Sconfig12.mkv',
                    'pure010_detectiongonogo_20220323_2Sounds_167_2Sconfig12.h5',
                    cameraParams)
 
 videos.add_session('2022-03-24', '2chords',
-                   'pure010_20220324_2Sounds_171_2Sconfig13.mp4',
+                   'pure010_20220324_2Sounds_171_2Sconfig13.mkv',
                    'pure010_detectiongonogo_20220324_2Sounds_171_2Sconfig13.h5',
                    cameraParams)
 
 videos.add_session('2022-03-24', '2chords',
-                   'pure010_20220324_2Sounds_172_2Sconfig13.mp4',
+                   'pure010_20220324_2Sounds_172_2Sconfig13.mkv',
                    'pure010_detectiongonogo_20220324_2Sounds_172_2Sconfig13.h5',
                    cameraParams)
 
 videos.add_session('2022-03-24', '2chords',
-                   'pure010_20220324_2Sounds_173_2Sconfig13.mp4',
+                   'pure010_20220324_2Sounds_173_2Sconfig13.mkv',
                    'pure010_detectiongonogo_20220324_2Sounds_173_2Sconfig13.h5',
                    cameraParams)
 
 videos.add_session('2022-03-25', '2chords',
-                   'pure010_20220325_2Sounds_177_2Sconfig14.mp4',
+                   'pure010_20220325_2Sounds_177_2Sconfig14.mkv',
                    'pure010_detectiongonogo_20220325_2Sounds_177_2Sconfig14.h5',
                    cameraParams)
 
 videos.add_session('2022-03-25', '2chords',
-                   'pure010_20220325_2Sounds_178_2Sconfig14.mp4',
+                   'pure010_20220325_2Sounds_178_2Sconfig14.mkv',
                    'pure010_detectiongonogo_20220325_2Sounds_178_2Sconfig14.h5',
                    cameraParams)
                    
 videos.add_session('2022-03-28', '2chords',
-                   'pure010_20220328_2Sounds_179_2Sconfig14.mp4',
+                   'pure010_20220328_2Sounds_179_2Sconfig14.mkv',
                    'pure010_detectiongonogo_20220328_2Sounds_179_2Sconfig14.h5',
                    cameraParams)                   
                                       
 videos.add_session('2022-03-31', 'multipleFreqs',
-                   'pure010_20220331_mfq_183_mconfig1.mp4',
+                   'pure010_20220331_mfq_183_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220331_mfq_183_mconfig1.h5',
                    cameraParams)
 
 videos.add_session('2022-03-31', 'mutlipleFreqs',
-                   'pure010_20220331_mfq_184_mconfig1',
+                   'pure010_20220331_mfq_184_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220331_mfq_183_mconfig1.h5',
                    cameraParams)
                    
 videos.add_session('2022-03-31', 'multipleFreqs',
-                   'pure010_20220331_mfq_185_mconfig1.mp4',
+                   'pure010_20220331_mfq_185_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220331_mfq_185_mconfig1.h5',
                    cameraParams)                   
                                       
 videos.add_session('2022-04-04', 'multipleFreqs',
-                   'pure010_20220404_mfq_186_mconfig1.mp4',
+                   'pure010_20220404_mfq_186_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220404_mfq_186_mconfig1.h5',
                    cameraParams)
 
 videos.add_session('2022-04-04', 'multipleFreqs',
-                   'pure010_20220404_mfq_187_mconfig1.mp4',
+                   'pure010_20220404_mfq_187_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220404_mfq_187_mconfig1.h5',
                    cameraParams)
 
 videos.add_session('2022-04-04', 'multipleFreqs',
-                   'pure010_20220404_mfq_188_mconfig1.mp4',
+                   'pure010_20220404_mfq_188_mconfig1.mkv',
                    'pure010_am_tuning_curve_20220404_mfq_188_mconfig1.h5',
                    cameraParams)                
                    
+videos.add_session('2022-04-15', 'freqRange',
+                   'pure010_20220415_xtremes_195_xconfig1.mkv',
+                   'pure010_detectiongonogo_pure010_20220415_xtremes_195_xconfig1.h5',
+                   cameraParams)
+
+videos.add_session('2022-04-18', 'freqRange',
+                   'pure010_20220418_xtremes_196_xconfig1.mkv',
+                   'pure010_detectiongonogo_xtremes_196_xconfig1.h5',
+                   cameraParams)
                    
-                   
-                   
-                   
-                   
-                   
-                   
-                   
-                   
-                   
-                   
+videos.add_session('2022-04-18', 'freqRange',
+                   'pure010_20220418_xtremes_197_xconfig1.mkv',
+                   'pure010_detectiongonogo_20220418_xtremes_197_xconfig1.h5',
+                   cameraParams)                   
+                                      
+videos.add_session('2022-04-20', 'freqRange',
+                   'pure010_20220420_xtremes_201_xconfig1.mkv',
+                   'pure010_detectiongonogo_20220420_xtremes_201_xconfig1.h5',
+                   cameraParams)
+
+videos.add_session('2022-04-21', 'freqRange',
+                   'pure010_20220421_xtremes_202_xconfig1.mkv',
+                   'pure010_detectiongonogo_20220421_xtremes_202_xconfig1.h5',
+                   cameraParams)
+
+videos.add_session('2022-04-21', 'freqRange',
+                   'pure010_20220421_xtremes_203_xconfig1.mkv',
+                   'pure010_detectiongonogo_20220421_xtremes_203_xconfig1.h5',
+                   cameraParams)                
+                                      
                    
                    
                    
