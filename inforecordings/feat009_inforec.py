a688ea7fb70293a58c19233ae801dd17aae33317
Jenny Mohn <jlmohn@ucdavis.edu>
2022-06-05 14:11:02 -0700

    updated feat009 inforec/infovid files

--- inforecordings/feat009_inforec.py
+++ inforecordings/feat009_inforec.py
@@ -14,10 +14,26 @@ experiments.append(exp0)
 # 14:55 in brain
 # 15:00 reached max depth
 # 15:19 started recording
-# 16: done
+# 16:26 done
 
 exp0.add_site(2949)
 exp0.maxDepth = 2949
 exp0.add_session('15-19-36','a','AM','am_tuning_curve')
 exp0.add_session('15-28-18','b','pureTones','am_tuning_curve')
 exp0.add_session('15-46-19','a','FTVOTBorders','2afc_speech')
+
+
+exp1 = celldatabase.Experiment(subject, '2022-06-05', brainArea='AC_left', probe = 'NPv1-4542', recordingTrack='anteromedial_DiD', info=['facesMedial', 'soundRight']) #reference = tip
+experiments.append(exp1)
+# 12:32 in booth
+# 12:34 in brain
+# 12:40 reached max depth. Very noisy today.
+# 12:55 started recording
+# 14:07 done
+
+exp1.add_site(2972)
+exp1.maxDepth = 2972
+#exp1.add_session('12-57-32','a','AM','am_tuning_curve') #presented binaurally on accident.
+exp1.add_session('13-06-26','b','pureTones','am_tuning_curve')
+exp1.add_session('13-23-36','a','FTVOTBorders','2afc_speech')
+exp1.add_session('13-59-18','c','AM','am_tuning_curve')
--- infovideos/feat009_infovideos.py
+++ infovideos/feat009_infovideos.py
@@ -6,7 +6,7 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 
 # Session parameters: date, sessionType, videoFile, behavFile, cameraParams
 
-# 2022-03-23
+# 2022-06-04
 
 videos.add_session('2022-06-04', 'AM',
                    'feat009_am_tuning_curve_20220604_01.mkv',
@@ -20,4 +20,23 @@ videos.add_session('2022-06-04', 'pureTones',
 videos.add_session('2022-06-04', 'FTVOTBorders',
                    'feat009_2afc_speech_20220604_01.mkv',
                    'feat009_2afc_speech_20220604a.h5', cameraParams) 
+                   
+# 2022-06-05
+
+videos.add_session('2022-06-05', 'AM',
+                   'feat009_am_tuning_curve_20220605_01.mkv',
+                   'feat009_am_tuning_curve_20220605a.h5', cameraParams) 
+
+
+videos.add_session('2022-06-05', 'pureTones',
+                   'feat009_am_tuning_curve_20220605_02.mkv',
+                   'feat009_am_tuning_curve_20220605b.h5', cameraParams) 
+
+videos.add_session('2022-06-05', 'FTVOTBorders',
+                   'feat009_2afc_speech_20220605_01.mkv',
+                   'feat009_2afc_speech_20220605a.h5', cameraParams) 
+                   
+videos.add_session('2022-06-05', 'AM',
+                   'feat009_am_tuning_curve_20220605_03.mkv',
+                   'feat009_am_tuning_curve_20220605c.h5', cameraParams) 
 
