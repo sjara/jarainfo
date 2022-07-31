e12db0c2dcdbc9740c67f506c07d91bde344b1f2
Jenny Mohn <jlmohn@ucdavis.edu>
2022-07-31 15:43:36 -0700

    created feat010_tracks file

---
+++ infohistology/feat010_tracks.py
@@ -0,0 +1,16 @@
+subject = 'feat010'
+tracks = [
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C5_L',
+     'recordingTrack':'anterolateral_DiD', 'shank':1, 'atlasZ':216}, #tip: p1_C2_L
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C3_L',
+     'recordingTrack':'anteromedial_DiI', 'shank':1, 'atlasZ':209},#tip: p1_B6_L
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B2_R',
+     'recordingTrack':'anteromedial_DiD', 'shank':1, 'atlasZ':184}, #tip: p1_B1_R
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B6_R',
+     'recordingTrack':'middlemedial_DiI', 'shank':1, 'atlasZ':200}, #tip: p1_B3_R
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_C2_R',
+     'recordingTrack':'posterior_DiD', 'shank':1, 'atlasZ':208}  ] #tip: p1_B5_R
