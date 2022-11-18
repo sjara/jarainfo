66ef8efc594cd3079065ce9511ecf1499634b283
Muhammad Haziq <jlmohn@ucdavis.edu>
2022-11-18 14:20:52 -0800

    added febe008_tracks file

---
+++ infohistology/febe008_tracks.py
@@ -0,0 +1,21 @@
+subject = 'febe008'
+tracks = [
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B5_R',
+     'recordingTrack':'caudomedial_DiD', 'shank':1, 'atlasZ':183}, #tip:
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_C1_R',
+     'recordingTrack':'caudomedial_DiI', 'shank':1, 'atlasZ':191},#tip: p1_B5_R
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B6_R',
+     'recordingTrack':'caudolateral_DiD', 'shank':1, 'atlasZ':189}, #tip: p1_B4_R
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B1_L',
+     'recordingTrack':'posterior_DiI', 'shank':1, 'atlasZ':163}, #tip: 
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B3_L',
+     'recordingTrack':'posterior_DiD', 'shank':1, 'atlasZ':178}, #tip: p1_B2_L
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_C2_L',
+     'recordingTrack':'anterior_DiI', 'shank':1, 'atlasZ':195}  ] #tip: p1_C4_L
+     
+     
