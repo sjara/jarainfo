ab69afdf3cf61b95abe1df6cbbdedc28f13a38e2
Jenny Mohn <jlmohn@ucdavis.edu>
2023-01-25 15:28:38 -0800

    fixed typo

--- infovideos/acid002_infovideos.py
+++ infovideos/acid002_infovideos.py
@@ -9,28 +9,28 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 # 2023-01-25
 
 videos.add_session('2023-01-25', 'prePureTones',
-                   'acid001_am_tuning_curve_20230125_01pre.mkv',
-                   'acid001_am_tuning_curve_20230125apre.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_01pre.mkv',
+                   'acid002_am_tuning_curve_20230125apre.h5', cameraParams) 
 
 videos.add_session('2023-01-25', 'postPureTones',
-                   'acid001_am_tuning_curve_20230125_01post.mkv',
-                   'acid001_am_tuning_curve_20230125apost.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_01post.mkv',
+                   'acid002_am_tuning_curve_20230125apost.h5', cameraParams) 
                    
   
 videos.add_session('2023-01-25', 'prePureTones',
-                   'acid001_am_tuning_curve_20230125_02pre.mkv',
-                   'acid001_am_tuning_curve_20230125bpre.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_02pre.mkv',
+                   'acid002_am_tuning_curve_20230125bpre.h5', cameraParams) 
 
 videos.add_session('2023-01-25', 'postPureTones',
-                   'acid001_am_tuning_curve_20230125_02post.mkv',
-                   'acid001_am_tuning_curve_20230125bpost.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_02post.mkv',
+                   'acid002_am_tuning_curve_20230125bpost.h5', cameraParams) 
 
 
 videos.add_session('2023-01-25', 'prePureTones',
-                   'acid001_am_tuning_curve_20230125_03pre.mkv',
-                   'acid001_am_tuning_curve_20230125cpre.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_03pre.mkv',
+                   'acid002_am_tuning_curve_20230125cpre.h5', cameraParams) 
 
 videos.add_session('2023-01-25', 'postPureTones',
-                   'acid001_am_tuning_curve_20230125_03post.mkv',
-                   'acid001_am_tuning_curve_20230125cpost.h5', cameraParams) 
+                   'acid002_am_tuning_curve_20230125_03post.mkv',
+                   'acid002_am_tuning_curve_20230125cpost.h5', cameraParams) 
                             
