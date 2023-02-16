fca2c8552b689b6783ab1e163c7378ae2ac78d93
Jenny Mohn <jlmohn@ucdavis.edu>
2023-02-16 12:32:40 -0800

    updated acid003 inforec

--- inforecordings/acid003_inforec.py
+++ inforecordings/acid003_inforec.py
@@ -62,3 +62,30 @@ exp1.add_session('11-07-04','cpre','prePureTones','am_tuning_curve')#n160
 # Injection Method 001- subq flank 11:11
 exp1.add_session('11-10-55','cduring','','')
 exp1.add_session('11-12-17','cpost','postPureTones','am_tuning_curve')#n160
+
+
+exp2 = celldatabase.Experiment(subject, '2023-02-16', brainArea='AC_right', probe='NPv1-4542', recordingTrack='posteriorlateral_DiI', info=['facesMedial', 'soundLeft']) #reference = tip
+experiments.append(exp2)
+#  10:30 in booth
+#  10:38 in brain
+#  10:39 reached max depth
+#  11:09 started recording
+#  12:23 done
+
+exp2.add_site(3050)
+exp2.maxDepth = 3050
+exp2.add_session('11-10-00','apre','prePureTones','am_tuning_curve') #n320
+exp2.add_session('11-19-14','apre','preHighFreq','oddball_sequence') #n500
+exp2.add_session('11-24-57','bpre','preLowFreq','oddball_sequence')
+exp2.add_session('11-30-43','cpre','preFM_Down','oddball_sequence')
+exp2.add_session('11-37-12','dpre','preFM_Up','oddball_sequence')
+
+#saline 0.1mL subq flank
+exp2.add_session('11-43-31','aduring','','')
+
+exp2.add_session('11-46-21','apost','postPureTones','am_tuning_curve') #accidentally ran ~450 trials
+exp2.add_session('11-57-51','apost','postHighFreq','oddball_sequence')
+exp2.add_session('12-03-54','bpost','postLowFreq','oddball_sequence')
+exp2.add_session('12-10-21','cpost','postFM_Down','oddball_sequence')
+exp2.add_session('12-16-59','dpost','postFM_Up','oddball_sequence')
+
--- infovideos/acid003_infovideos.py
+++ infovideos/acid003_infovideos.py
@@ -61,4 +61,50 @@ videos.add_session('2023-02-15', 'prePureTones',
 
 videos.add_session('2023-02-15', 'postPureTones',
                    'acid003_am_tuning_curve_20230215_03post.mkv',
-                   'acid003_am_tuning_curve_20230215cpost.h5', cameraParams)                             
+                   'acid003_am_tuning_curve_20230215cpost.h5', cameraParams)
+                   
+
+# 2023-02-16
+
+videos.add_session('2023-02-16', 'prePureTones',
+                   'acid003_am_tuning_curve_20230216_01pre.mkv',
+                   'acid003_am_tuning_curve_20230216apre.h5', cameraParams) 
+
+videos.add_session('2023-02-16', 'preHighFreq',
+                   'acid003_oddball_sequence_20230216_01pre.mkv',
+                   'acid003_oddball_sequence_20230216apre', cameraParams) 
+
+videos.add_session('2023-02-16', 'preLowFreq',
+                   'acid003_oddball_sequence_20230216_02pre.mkv',
+                   'acid003_oddball_sequence_20230216bpre', cameraParams)
+
+videos.add_session('2023-02-16', 'preFM_Down',
+                   'acid003_oddball_sequence_20230216_03pre.mkv',
+                   'acid003_oddball_sequence_20230216cpre', cameraParams)
+                   
+videos.add_session('2023-02-16', 'preFM_Up',
+                   'acid003_oddball_sequence_20230216_04pre.mkv',
+                   'acid003_oddball_sequence_20230216dpre', cameraParams)
+# Saline
+
+videos.add_session('2023-02-16', 'postPureTones',
+                   'acid003_am_tuning_curve_20230216_01post.mkv',
+                   'acid003_am_tuning_curve_20230216apost.h5', cameraParams) 
+
+videos.add_session('2023-02-16', 'postHighFreq',
+                   'acid003_oddball_sequence_20230216_01post.mkv',
+                   'acid003_oddball_sequence_20230216apost', cameraParams) 
+
+videos.add_session('2023-02-16', 'postLowFreq',
+                   'acid003_oddball_sequence_20230216_02post.mkv',
+                   'acid003_oddball_sequence_20230216bpost', cameraParams)
+
+videos.add_session('2023-02-16', 'postFM_Down',
+                   'acid003_oddball_sequence_20230216_03post.mkv',
+                   'acid003_oddball_sequence_20230216cpost', cameraParams)
+                   
+videos.add_session('2023-02-16', 'postFM_Up',
+                   'acid003_oddball_sequence_20230216_04post.mkv',
+                   'acid003_oddball_sequence_20230216dpost', cameraParams)
+                   
+                             
