cd41f12609d0fcc2c2cb141b54a9b352b00d10c5
jaratask2 <mhorrock@uoregon.edu>
2023-08-08 15:58:41 -0700

    updated acid009

--- inforecordings/acid009_inforec.py
+++ inforecordings/acid009_inforec.py
@@ -49,4 +49,43 @@ exp0.add_session('14-03-30','ddoi','doiFM_Up','oddball_sequence') #n501
 exp0.add_session('14-09-44','','doiSpontaneous','')
 
 
+# 2023-08-08
+
+exp1 = celldatabase.Experiment(subject, '2023-08-08', brainArea='AC_left', probe='NPv1-2641', recordingTrack='implant_DiD', info=['facesPosterior', 'soundRight']) #reference = tip
+experiments.append(exp1)
+
+# Experiment Notes:
+#  13:45 in booth
+#  13:47 started recording
+#  16:07 done
+
+exp1.maxDepth = 3000
+
+exp1.add_site(3000) 
+exp1.add_session('13-48-14','apre','prePureTones','am_tuning_curve') #n321
+exp1.add_session('13-56-13','apre','preHighFreq','oddball_sequence') #n501
+exp1.add_session('14-02-30','bpre','preLowFreq','oddball_sequence') #n501
+exp1.add_session('14-09-17','cpre','preFM_Down','oddball_sequence') #n501
+exp1.add_session('14-15-43','dpre','preFM_Up','oddball_sequence') #n501
+
+#saline "0.11mL" subq flank- 15min
+exp1.add_session('14-22-06','','salineInjection','')
+
+exp1.add_session('14-37-25','asaline','salinePureTones','am_tuning_curve') #n321
+exp1.add_session('14-45-25','asaline','salineHighFreq','oddball_sequence') #n501
+exp1.add_session('14-51-01','bsaline','salineLowFreq','oddball_sequence') #n501
+exp1.add_session('14-56-42','csaline','salineFM_Down','oddball_sequence') #n501
+exp1.add_session('15-03-11','dsaline','salineFM_Up','oddball_sequence') #n501
+
+#doi "0.11mL" subq flank 15min
+exp1.add_session('15-09-33','','doiInjection','')
+
+exp1.add_session('15-24-56','adoi','doiPureTones','am_tuning_curve') #n321
+exp1.add_session('15-32-54','adoi','doiHighFreq','oddball_sequence') #n501
+exp1.add_session('15-38-33','bdoi','doiLowFreq','oddball_sequence') #n501
+exp1.add_session('15-44-11','cdoi','doiFM_Down','oddball_sequence') #n501
+exp1.add_session('15-50-39','ddoi','doiFM_Up','oddball_sequence') #n501
+
+#doi spontaneous recording 10min
+exp1.add_session('15-57-01','','doiSpontaneous','')
 
--- infovideos/acid009_infovideos.py
+++ infovideos/acid009_infovideos.py
@@ -84,5 +84,83 @@ videos.add_session('2023-08-04', 'doiFM_Up',
 #DOI Spontaneous                   
 videos.add_session('2023-08-04', 'doiSpontaneous',
                    'acid009_20230804_doiSpontaneous.mkv','', cameraParams)
+
                    
-                                                                                       
+# EVERY SESSION AFTER HERE HAS A 1 SECOND MANUAL SYNC LIGHT BEFORE AND AFTER THE TRIALS.
+# 2023-08-08
+
+videos.add_session('2023-08-08', 'prePureTones',
+                   'acid009_am_tuning_curve_20230808_01pre.mkv',
+                   'acid009_am_tuning_curve_20230808apre.h5', cameraParams) 
+
+videos.add_session('2023-08-08', 'preHighFreq',
+                   'acid009_oddball_sequence_20230808_01pre.mkv',
+                   'acid009_oddball_sequence_20230808apre', cameraParams) 
+
+videos.add_session('2023-08-08', 'preLowFreq',
+                   'acid009_oddball_sequence_20230808_02pre.mkv',
+                   'acid009_oddball_sequence_20230808bpre', cameraParams)
+
+videos.add_session('2023-08-08', 'preFM_Down',
+                   'acid009_oddball_sequence_20230808_03pre.mkv',
+                   'acid009_oddball_sequence_20230808cpre', cameraParams)
+                   
+videos.add_session('2023-08-08', 'preFM_Up',
+                   'acid009_oddball_sequence_20230808_04pre.mkv',
+                   'acid009_oddball_sequence_20230808dpre', cameraParams)
+# Saline
+videos.add_session('2023-08-08', 'salineInjection',
+                   'acid009_20230808_salineInjection.mkv',
+                   '', cameraParams)
+                     
+
+videos.add_session('2023-08-08', 'salinePureTones',
+                   'acid009_am_tuning_curve_20230808_01saline.mkv',
+                   'acid009_am_tuning_curve_20230808asaline.h5', cameraParams) 
+
+videos.add_session('2023-08-08', 'salineHighFreq',
+                   'acid009_oddball_sequence_20230808_01saline.mkv',
+                   'acid009_oddball_sequence_20230808asaline', cameraParams) 
+
+videos.add_session('2023-08-08', 'salineLowFreq',
+                   'acid009_oddball_sequence_20230808_02saline.mkv',
+                   'acid009_oddball_sequence_20230808bsaline', cameraParams)
+
+videos.add_session('2023-08-08', 'salineFM_Down',
+                   'acid009_oddball_sequence_20230808_03saline.mkv',
+                   'acid009_oddball_sequence_20230808csaline', cameraParams)
+                   
+videos.add_session('2023-08-08', 'salineFM_Up',
+                   'acid009_oddball_sequence_20230808_04saline.mkv',
+                   'acid009_oddball_sequence_20230808dsaline', cameraParams)
+                   
+# DOI
+videos.add_session('2023-08-08', 'doiInjection',
+                   'acid009_20230808_doiInjection.mkv',
+                   '', cameraParams)
+                        
+
+videos.add_session('2023-08-08', 'doiPureTones',
+                   'acid009_am_tuning_curve_20230808_01doi.mkv',
+                   'acid009_am_tuning_curve_20230808adoi.h5', cameraParams) 
+
+videos.add_session('2023-08-08', 'doiHighFreq',
+                   'acid009_oddball_sequence_20230808_01doi.mkv',
+                   'acid009_oddball_sequence_20230808adoi', cameraParams) 
+
+videos.add_session('2023-08-08', 'doiLowFreq',
+                   'acid009_oddball_sequence_20230808_02doi.mkv',
+                   'acid009_oddball_sequence_20230808bdoi', cameraParams)
+
+videos.add_session('2023-08-08', 'doiFM_Down',
+                   'acid009_oddball_sequence_20230808_03doi.mkv',
+                   'acid009_oddball_sequence_20230808cdoi', cameraParams)
+                   
+videos.add_session('2023-08-08', 'doiFM_Up',
+                   'acid009_oddball_sequence_20230808_04doi.mkv',
+                   'acid009_oddball_sequence_20230808ddoi', cameraParams)                   
+
+#DOI Spontaneous                   
+videos.add_session('2023-08-08', 'doiSpontaneous',
+                   'acid009_20230808_doiSpontaneous.mkv','', cameraParams)
+                                                                                                          
