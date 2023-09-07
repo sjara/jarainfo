fe15c912fe5bae6e87cab4cb072f9ee6dcec988e
Max Horrocks <max.horrocks@uoregon.edu>
2023-09-07 10:03:05 -0700

    added acid006x007 track files

---
+++ infohistology/acid006_tracks.py
@@ -0,0 +1,3 @@
+tracks = [
+	{'subject':'acid006', 'brainArea':'AC_right', 'histImage':'p1_c2_R', 'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 185} #tip: p1_c5_R
+	]
---
+++ infohistology/acid007_tracks.py
@@ -0,0 +1,3 @@
+tracks = [
+	{'subject':'acid007', 'brainArea':'AC_right', 'histImage':'p1_c3_R', 'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 210}
+	]
