b60f8d16d21fbd8c2fc059738da4da954e9c5a3f
jaratask2 <>
2024-02-26 17:02:16 -0800

    first video recording for feat015

--- infovideos/feat015_infovideos.py
+++ infovideos/feat015_infovideos.py
@@ -1,1 +1,17 @@
+from jaratoolbox import videoinfo
 
+subject = 'feat015'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+# Session parameters: date, sessionType, videoFile, behavFile, cameraParams
+
+# 2024-02-23
+
+videos.add_session('2024-02-23', 'pureTones',
+                   'feat015_am_tuning_curve_20240223_01.mkv',
+                   'feat015_am_tuning_curve_20240223a.h5', cameraParams) 
+                   
+videos.add_session('2024-02-23', 'AM',
+                   'feat015_am_tuning_curve_20240223_02.mkv',
+                   'feat015_am_tuning_curve_20240223b.h5', cameraParams)                    
