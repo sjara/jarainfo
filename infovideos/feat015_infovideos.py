038fd76731d8c2adba5263bfad0f1e600bf30e7a
jaratask2 <>
2024-03-22 12:58:14 -0700

    video info for most recent recording

--- infovideos/feat015_infovideos.py
+++ infovideos/feat015_infovideos.py
@@ -54,4 +54,38 @@ videos.add_session('2024-03-06', 'pureTones',
                    
 videos.add_session('2024-03-06', 'AM',
                    'feat015_am_tuning_curve_20240306_02.mkv',
-                   'feat015_am_tuning_curve_20240306b.h5', cameraParams)                                                   
+                   'feat015_am_tuning_curve_20240306b.h5', cameraParams)  
+                   
+#2024-03-20
+
+# Night mode flickered in and out. Covered one of the lights, 3 originally were uncovered and this appeared to fix the issue.
+# Left speaker was not working - we switched out the cable from the right side to the left and this fixed the issue. NOTE: the behavior file will report that the sound was presented to the right side but it was presented LEFT.
+
+videos.add_session('2024-03-20', 'pureTones',
+                   'feat015_am_tuning_curve_20240320_01.mkv',
+                   'feat015_am_tuning_curve_20240320a.h5', cameraParams) 
+                   
+videos.add_session('2024-03-20', 'AM',
+                   'feat015_am_tuning_curve_20240320_02.mkv',
+                   'feat015_am_tuning_curve_20240320b.h5', cameraParams)   
+                   
+videos.add_session('2024-03-20', 'naturalSounds',
+                   'feat015_natural_sound_detection_20240320_01.mkv',
+                   'feat015_natural_sound_detection_20240320a.h5', cameraParams) 
+                   
+#2024-03-21
+
+# Night mode flickered in and out - pressing the sensor cover seemed to fix
+
+
+videos.add_session('2024-03-21', 'pureTones',
+                   'feat015_am_tuning_curve_20240321_01.mkv',
+                   'feat015_am_tuning_curve_20240321a.h5', cameraParams) 
+                   
+videos.add_session('2024-03-21', 'AM',
+                   'feat015_am_tuning_curve_20240321_02.mkv',
+                   'feat015_am_tuning_curve_20240321b.h5', cameraParams)   
+                   
+videos.add_session('2024-03-21', 'naturalSounds',
+                   'feat015_natural_sound_detection_20240321_01.mkv',
+                   'feat015_natural_sound_detection_20240321a.h5', cameraParams)                                                           
