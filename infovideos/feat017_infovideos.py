abe2aa5e9263403397e74d25bba86d56f7106fc0
jaratask2 <>
2024-04-02 10:57:19 -0700

    video info for feat017 2024-04-02

--- infovideos/feat017_infovideos.py
+++ infovideos/feat017_infovideos.py
@@ -48,5 +48,46 @@ videos.add_session('2024-03-27', 'naturalSounds',
                    'feat017_natural_sound_detection_20240327_01.mkv',
                    'feat017_natural_sound_detection_20240327a.h5', cameraParams) 
                    
-#2024-03-28
+#2024-03-29
+
+videos.add_session('2024-03-29', 'pureTones',
+                   'feat017_am_tuning_curve_20240329_01.mkv',
+                   'feat017_am_tuning_curve_20240329a.h5', cameraParams) 
+                   
+videos.add_session('2024-03-29', 'AM',
+                   'feat017_am_tuning_curve_20240329_02.mkv',
+                   'feat017_am_tuning_curve_20240329b.h5', cameraParams)  
+                   
+videos.add_session('2024-03-29', 'naturalSounds',
+                   'feat017_natural_sound_detection_20240329_01.mkv',
+                   'feat017_natural_sound_detection_20240329a.h5', cameraParams) 
+                   
+#2024-04-01
+
+videos.add_session('2024-04-01', 'pureTones',
+                   'feat017_am_tuning_curve_20240401_01.mkv',
+                   'feat017_am_tuning_curve_20240401a.h5', cameraParams) 
+                   
+videos.add_session('2024-04-01', 'AM',
+                   'feat017_am_tuning_curve_20240401_02.mkv',
+                   'feat017_am_tuning_curve_202400401b.h5', cameraParams)  
+                   
+videos.add_session('2024-04-01', 'naturalSounds',
+                   'feat017_natural_sound_detection_20240401_01.mkv',
+                   'feat017_natural_sound_detection_20240401a.h5', cameraParams) 
+                   
+#2024-04-02
+
+videos.add_session('2024-04-02', 'pureTones',
+                   'feat017_am_tuning_curve_20240402_01.mkv',
+                   'feat017_am_tuning_curve_20240402a.h5', cameraParams) 
+                   
+videos.add_session('2024-04-02', 'AM',
+                   'feat017_am_tuning_curve_20240402_02.mkv',
+                   'feat017_am_tuning_curve_202400402b.h5', cameraParams)  
+                   
+videos.add_session('2024-04-02', 'naturalSounds',
+                   'feat017_natural_sound_detection_20240402_01.mkv',
+                   'feat017_natural_sound_detection_20240402a.h5', cameraParams) 
+
 
