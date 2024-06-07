d2992aadc8474e204f5eda1aa1e0e24d70c8b172
jaratask2 <>
2024-06-07 12:51:00 -0700

    video info for feat018 second day of recording

--- infovideos/feat018_infovideos.py
+++ infovideos/feat018_infovideos.py
@@ -20,3 +20,19 @@ videos.add_session('2024-06-06', 'naturalSounds',
 videos.add_session('2024-06-06', 'AM',
                    'feat018_am_tuning_curve_20240606_02.mkv',
                    'feat018_am_tuning_curve_20240606b.h5', cameraParams) 
+                   
+                   
+# 2024-06-07
+
+videos.add_session('2024-06-07', 'pureTones',
+                   'feat018_am_tuning_curve_20240607_01.mkv',
+                   'feat018_am_tuning_curve_20240607a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2024-06-07', 'naturalSounds',
+                   'feat018_natural_sound_detection_20240607_01.mkv',
+                   'feat018_natural_sound_detection_20240607a.h5', cameraParams) 
+                   
+videos.add_session('2024-06-07', 'AM',
+                   'feat018_am_tuning_curve_20240607_02.mkv',
+                   'feat018_am_tuning_curve_20240607b.h5', cameraParams) 
