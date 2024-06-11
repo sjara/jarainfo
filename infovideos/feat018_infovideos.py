29717144be906eed049a96879218e42f633a9269
jaratask2 <>
2024-06-11 11:16:23 -0700

    latest video and behavior for feat018

--- infovideos/feat018_infovideos.py
+++ infovideos/feat018_infovideos.py
@@ -36,3 +36,36 @@ videos.add_session('2024-06-07', 'naturalSounds',
 videos.add_session('2024-06-07', 'AM',
                    'feat018_am_tuning_curve_20240607_02.mkv',
                    'feat018_am_tuning_curve_20240607b.h5', cameraParams) 
+                   
+# 2024-06-10
+
+videos.add_session('2024-06-10', 'pureTones',
+                   'feat018_am_tuning_curve_20240610_01.mkv',
+                   'feat018_am_tuning_curve_20240610a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2024-06-10', 'naturalSounds',
+                   'feat018_natural_sound_detection_20240610_01.mkv',
+                   'feat018_natural_sound_detection_20240610a.h5', cameraParams) 
+                   
+videos.add_session('2024-06-10', 'AM',
+                   'feat018_am_tuning_curve_20240610_02.mkv',
+                   'feat018_am_tuning_curve_20240610b.h5', cameraParams) 
+                   
+# 2024-06-11
+
+videos.add_session('2024-06-11', 'pureTones',
+                   'feat018_am_tuning_curve_20240611_01.mkv',
+                   'feat018_am_tuning_curve_20240611a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2024-06-11', 'naturalSounds',
+                   'feat018_natural_sound_detection_20240611_01.mkv',
+                   'feat018_natural_sound_detection_20240611a.h5', cameraParams) 
+                   
+videos.add_session('2024-06-11', 'AM',
+                   'feat018_am_tuning_curve_20240611_02.mkv',
+                   'feat018_am_tuning_curve_20240611b.h5', cameraParams) 
+                   
+                   
+                   
