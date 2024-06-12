ac161ce027af347a57e39c201d54038788136d31
jaratask2 <>
2024-06-12 15:49:11 -0700

    feat019 infovideo

---
+++ infovideos/feat019_infovideos.py
@@ -0,0 +1,35 @@
+from jaratoolbox import videoinfo
+
+subject = 'feat019'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+# Session parameters: date, sessionType, videoFile, behavFile, cameraParams
+
+# 2024-06-12
+
+videos.add_session('2024-06-12', 'pureTones',
+                   'feat019_am_tuning_curve_20240612_01.mkv',
+                   'feat019_am_tuning_curve_20240612a.h5', cameraParams) 
+videos.add_session('2024-06-12', 'naturalSound',
+                   'feat019_natural_sound_detection_20240612_01.mkv',
+                   'feat019_natural_sound_detection_20240612a.h5', cameraParams) 
+videos.add_session('2024-06-12', 'AM',
+                   'feat019_am_tuning_curve_20240612_02.mkv',
+                   'feat019_am_tuning_curve_20240612b.h5', cameraParams) 
+
+
+# 2024-06-xx
+
+#videos.add_session('2024-06-xx', 'pureTones',
+#                   'feat019_am_tuning_curve_202406xx_01.mkv',
+#                   'feat019_am_tuning_curve_202406xxa.h5', cameraParams) 
+#videos.add_session('2024-06-xx', 'naturalSound',
+#                   'feat019_natural_sound_detection_202406xx_01.mkv',
+#                   'feat019_natural_sound_detection_202406xxa.h5', cameraParams) 
+#videos.add_session('2024-06-xx', 'AM',
+#                   'feat019_am_tuning_curve_202406xx_02.mkv',
+#                   'feat019_am_tuning_curve_202406xxb.h5', cameraParams) 
+
+
+
