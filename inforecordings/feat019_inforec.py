449fb4e878232f109d9cf4c43afb675c222ec9c5
jaratask2 <>
2024-06-13 16:37:33 -0700

    06-13 recording

--- inforecordings/feat019_inforec.py
+++ inforecordings/feat019_inforec.py
@@ -27,8 +27,29 @@ exp0.add_session('14-56-59', 'a', 'naturalSound', 'natural_sound_detection')
 exp0.add_session('15-24-35', 'b', 'AM', 'am_tuning_curve')
 
 
-#exp1 = celldatabase.Experiment(subject, '2024-06-xx', brainArea='left_AC', probe='NPv1-4662', recordingTrack='anteriorlateral_DiD', info=['facesMedial', 'soundRight']) # Reference = tip.
-#experiments.append(exp1)
+exp1 = celldatabase.Experiment(subject, '2024-06-13', brainArea='left_AC', probe='NPv1-4432', recordingTrack='posteriormedial_DiD', info=['facesMedial', 'soundRight']) # Reference = tip.
+experiments.append(exp1)
+# moved the penetration slightly anterior as probe would not penetrate at most posterior position
+
+# 14:45 in booth
+# 15:00 in brain
+# 15:10 reached max depth
+# 15:35 started recording pureTones
+# 15:34 done
+# 15:44 starting AM
+# 16:11 done
+# 16:12 started natural sounds
+# 16:20 done
+
+exp1.add_site(3000)
+exp1.maxDepth = 3000
+exp1.add_session('15-35-29', 'a', 'pureTones', 'am_tuning_curve')
+exp1.add_session('15-44-15', 'a', 'naturalSound', 'natural_sound_detection')
+exp1.add_session('16-12-36', 'b', 'AM', 'am_tuning_curve')
+
+
+#exp2 = celldatabase.Experiment(subject, '2024-06-xx', brainArea='left_AC', probe='NPv1-4432', recordingTrack='anteriorlateral_DiD', info=['facesMedial', 'soundRight']) # Reference = tip.
+#experiments.append(exp2)
 
 #  in booth
 #  in brain
@@ -40,11 +61,10 @@ exp0.add_session('15-24-35', 'b', 'AM', 'am_tuning_curve')
 #  started natural sounds
 #  done
 
-#exp1.add_site(3000)
-#exp1.maxDepth = 3000
-#exp1.add_session('xx-xx-xx', 'a', 'pureTones', 'am_tuning_curve')
-#exp1.add_session('xx-xx-xx', 'a', 'naturalSound', 'natural_sound_detection')
-#exp1.add_session('xx-xx-xx', 'b', 'AM', 'am_tuning_curve')
-
+#exp2.add_site(3000)
+#exp2.maxDepth = 3000
+#exp2.add_session('xx-xx-xx', 'a', 'pureTones', 'am_tuning_curve')
+#exp2.add_session('xx-xx-xx', 'a', 'naturalSound', 'natural_sound_detection')
+#exp2.add_session('xx-xx-xx', 'b', 'AM', 'am_tuning_curve')
 
 
