0e46f841e394247a64ead7ad645b3d5604ac7080
jaratask2 <>
2024-07-29 15:10:45 -0700

    test143 inforec file

--- inforecordings/test143_inforec.py
+++ inforecordings/test143_inforec.py
@@ -35,3 +35,30 @@ exp0.maxDepth = 3000
 # 17:00 started recording
 # 18:45 finished recording
 # 18:51 probe out of brain
+
+exp1 = celldatabase.Experiment(subject, '2024-07-26', brainArea='left_pStr', probe = 'NPv2-0282', recordingTrack='posteriorcoronal_DiI', info=['facesanterior', 'soundRight'])
+experiments.append(exp1)
+
+exp1.add_site(3000)
+# Reference = 2:tip
+# Electrode preset = All Shanks 1-96
+exp1.add_session('16-27-43','a','AM','am_tuning_curve')
+exp1.add_session('16-39-03','b','pureTones','am_tuning_curve')
+exp1.add_session('16-50-44','a','naturalSound','natural_sound_detection')
+
+exp1.add_site(?)
+# Reference = 2:tip
+# Electrode preset = All Shanks 97-192
+exp1.add_session('17-22-34','c','AM','am_tuning_curve')
+exp1.add_session('17-31-33','d','pureTones','am_tuning_curve')
+exp1.add_session('17-40-55','b','naturalSound','natural_sound_detection')
+exp1.maxDepth = 3000
+
+# 15:07 animal in the rig
+# 15:15 sylgard off
+# 15:48 probe in the brain
+# 15:55 ground wire in the well
+# 16:25 started recording with shanks 1-96
+# 17:22 recording with shanks 97-192
+# 18:12 finished recording
+# 18:19 probe out of brain
