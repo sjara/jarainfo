351b03a7c80544bd10e95eb7b61639d443101f50
jarauser <jarauser@jarastation4>
2024-08-13 13:20:16 -0700

    fixed nomenclature

---
+++ infohistology/feat018_tracks.py
@@ -0,0 +1,33 @@
+subject = 'feat018'
+tracks = [
+# Left hemisphere
+
+# {'subject':'feat018', 'brainArea':'xxx_AC', 'histImage':'xx-xx-xx', 'recordingTrack':'xX_DiX', 'shank':1, 'atlasZ':xxx}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-B3-01', 'recordingTrack':'posteriorLateral_DiD', 'shank':1, 'atlasZ':187}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-B4-01', 'recordingTrack':'posteriorMedial_DiI', 'shank':1, 'atlasZ':189}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-C1-01', 'recordingTrack':'centerLateral_DiI', 'shank':1, 'atlasZ':199}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-C2-01', 'recordingTrack':'centerMedial_DiD', 'shank':1, 'atlasZ':205}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-C4-01', 'recordingTrack':'anteriorLateral_DiO', 'shank':1, 'atlasZ':213}, 
+
+{'subject':'feat018', 'brainArea':'left_AC', 'histImage':'p1-C5-01', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':219}, 
+
+# Right hemisphere
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-B6-02', 'recordingTrack':'posteriorLateral_DiO', 'shank':1, 'atlasZ':197}, 
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-B6-02', 'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':197}, 
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-C4-02', 'recordingTrack':'centerLateral_DiD', 'shank':1, 'atlasZ':211}, 
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-C6-02', 'recordingTrack':'centerMedial_DiI', 'shank':1, 'atlasZ':216}, 
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-D3-02', 'recordingTrack':'anteriorLateral_DiI', 'shank':1, 'atlasZ':229}, 
+
+{'subject':'feat018', 'brainArea':'right_AC', 'histImage':'p1-D3-02', 'recordingTrack':'anteriorCenter_DiD', 'shank':1, 'atlasZ':229}, 
+
+]
