4d6cea3d00015ecef8950a35fa2962dc00dd687b
jarauser <jarauser@jarastation4>
2024-08-13 12:48:54 -0700

    fixed nomenclature

---
+++ infohistology/feat015_tracks.py
@@ -0,0 +1,25 @@
+subject = 'feat015'
+tracks = [
+   #right hemisphere 
+ {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-C1-01', 'recordingTrack':'posteriorMedial_DiI', 'shank':1, 'atlasZ':194}, 
+ 
+  {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-D4-01', 'recordingTrack':'anteriorMedial_DiO', 'shank':1, 'atlasZ':234}, 
+  # Note that this penetration is really only visible where the probe entered the brain. It is difficult to determine where the probe tip ended up in the brain.
+  
+   {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-C5-01', 'recordingTrack':'posteriorCenter_DiD', 'shank':1, 'atlasZ':220}, 
+   
+    {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-C2-01', 'recordingTrack':'centerMedial_DiO', 'shank':1, 'atlasZ':196}, 
+    # Note that this penetration is very faint and there is really only evidence of DiO dye where the probe penetrated the brain. It is difficult to determine where the tip ended up in the brain. 
+    
+    #natural sounds paradigm introduced here
+    
+     {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-D3-01', 'recordingTrack':'anteriorLateral_DiI', 'shank':1, 'atlasZ':233}, 
+     
+      {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-C6-01', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':224}, 
+      
+       {'subject':'feat015', 'brainArea':'right_AC', 'histImage':'p1-D3-01', 'recordingTrack':'anteriorCenter_DiD', 'shank':1, 'atlasZ':233}, 
+       
+ ]
+ 
+ 
+ 
