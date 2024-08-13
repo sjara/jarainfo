d0a7d96f003dbf93e842b4e78cc6a1584fe10f7a
jarauser <jarauser@jarastation4>
2024-08-13 12:46:05 -0700

    fixed nomenclature

---
+++ infohistology/feat014_tracks.py
@@ -0,0 +1,12 @@
+subject = 'feat014'
+tracks = [
+   #right hemisphere 
+ {'subject':'feat014', 'brainArea':'right_AC', 'histImage':'p1-C3-02', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':197},
+ 
+    #left hemisphere 
+ {'subject':'feat014', 'brainArea':'left_AC', 'histImage':'p1-B6-01', 'recordingTrack':'posteriorLateral_DiO', 'shank':1, 'atlasZ':192},
+ 
+ {'subject':'feat014', 'brainArea':'left_AC', 'histImage':'p1-B6-01', 'recordingTrack':'anteriorCenter_DiI', 'shank':1, 'atlasZ':192},
+ 
+ {'subject':'feat014', 'brainArea':'left_AC', 'histImage':'p1-B6-01', 'recordingTrack':'anteriorMedial_DiD', 'shank':1, 'atlasZ':192},
+   ]
