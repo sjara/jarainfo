549ec650933723ca8f45e1379e9859c11632a083
jarauser <jarauser@jarastation4>
2024-08-13 13:10:46 -0700

    fixed nomenclature

---
+++ infohistology/feat017_tracks.py
@@ -0,0 +1,44 @@
+subject = 'feat017'
+tracks = [
+# Left hemisphere
+
+# {'subject':'feat017', 'brainArea':'xxx_AC', 'histImage':'xx-xx-xx', 'recordingTrack':'xX_DiX', 'shank':1, 'atlasZ':xxx}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-A2-01', 'recordingTrack':'posteriorMedial_DiO', 'shank':1, 'atlasZ':147}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-A4-01', 'recordingTrack':'posteriorLateral_DiO', 'shank':1, 'atlasZ':159}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-A2-01', 'recordingTrack':'posteriorCenter_DiI', 'shank':1, 'atlasZ':147}, 
+ #faint penetration 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-B1-01', 'recordingTrack':'centerCenter_DiD', 'shank':1, 'atlasZ':168}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-B4-01', 'recordingTrack':'centerMedial_DiI', 'shank':1, 'atlasZ':179}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-B5-01', 'recordingTrack':'anteriorLateral_DiI', 'shank':1, 'atlasZ':185}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-C5-01', 'recordingTrack':'anteriorCenter_DiO', 'shank':1, 'atlasZ':209}, 
+
+ {'subject':'feat017', 'brainArea':'left_AC', 'histImage':'p1-C3-01', 'recordingTrack':'anteriorMedial_DiD', 'shank':1, 'atlasZ':205}, 
+ 
+ #Right hemisphere
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-A6-02', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':151}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-A6-02', 'recordingTrack':'posteriorCenter_DiO', 'shank':1, 'atlasZ':151}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-B1-02', 'recordingTrack':'centerMedial_DiI', 'shank':1, 'atlasZ':156}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-B6-02', 'recordingTrack':'centerCenter_DiD', 'shank':1, 'atlasZ':181}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-B6-02', 'recordingTrack':'centerLateral_DiO', 'shank':1, 'atlasZ':181}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-D1-02', 'recordingTrack':'anteriorMedial_DiO', 'shank':1, 'atlasZ':210}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-D4-02', 'recordingTrack':'anteriorCenter_DiI', 'shank':1, 'atlasZ':219}, 
+
+ {'subject':'feat017', 'brainArea':'right_AC', 'histImage':'p1-D3-02', 'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':217}, 
+
+
+
+]
