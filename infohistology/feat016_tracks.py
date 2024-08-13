1f613e392d4c8b83430af6f698a27e6837dd4844
jarauser <jarauser@jarastation4>
2024-08-13 13:04:05 -0700

    fixed nomenclature

--- infohistology/feat016_tracks.py
+++ infohistology/feat016_tracks.py
@@ -4,30 +4,30 @@ tracks = [
 # {'subject':'feat0xx', 'brainArea':'AC_xxx', 'histImage':'xx-xx-xx', 'recordingTrack':'xX_DiX', 'shank':1, 'atlasZ':xxx}, 
 
 # left hemisphere
-    {'subject':'feat016', 'brainArea':'AC_left', 'histImage':'p1-C2-01',
+    {'subject':'feat016', 'brainArea':'left_AC', 'histImage':'p1-C2-01',
      'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':199},
      
-     {'subject':'feat016', 'brainArea':'AC_left', 'histImage':'p1-B3-01', 'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':177}, 
+     {'subject':'feat016', 'brainArea':'left_AC', 'histImage':'p1-B3-01', 'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':177}, 
      
-     {'subject':'feat016', 'brainArea':'AC_left', 'histImage':'p1-C1-01', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':196}, 
+     {'subject':'feat016', 'brainArea':'left_AC', 'histImage':'p1-C1-01', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':196}, 
      
-     {'subject':'feat016', 'brainArea':'AC_left', 'histImage':'p1-C2-01', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':199}, 
+     {'subject':'feat016', 'brainArea':'left_AC', 'histImage':'p1-C2-01', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':199}, 
      
-     {'subject':'feat016', 'brainArea':'AC_left', 'histImage':'p1-C5-01', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':212}, 
+     {'subject':'feat016', 'brainArea':'left_AC', 'histImage':'p1-C5-01', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':212}, 
      
  #right hemisphere   
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-B6-02', 'recordingTrack':'posteriorMedial_DiO', 'shank':1, 'atlasZ':194}, 
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-B6-02', 'recordingTrack':'posteriorMedial_DiO', 'shank':1, 'atlasZ':194}, 
  
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-C4-02', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':206}, 
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-C4-02', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':206}, 
  
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-C3-02', 'recordingTrack':'centerMedial_DiD', 'shank':1, 'atlasZ':202}, 
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-C3-02', 'recordingTrack':'centerMedial_DiD', 'shank':1, 'atlasZ':202}, 
  
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-D1-02', 'recordingTrack':'centerLateral_DiO', 'shank':1, 'atlasZ':220}, 
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-D1-02', 'recordingTrack':'centerLateral_DiO', 'shank':1, 'atlasZ':220}, 
   # Note that this penetration is faint there is also a potential DiO track that is more lateral, but this track may be from a DiI penetration, as evidenced by loss of color when turning up the red filter. 
  
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-C6-02', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':216}, 
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-C6-02', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':216}, 
  
-  {'subject':'feat016', 'brainArea':'AC_right', 'histImage':'p1-D1-02', 'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':220},  
+  {'subject':'feat016', 'brainArea':'right_AC', 'histImage':'p1-D1-02', 'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':220},  
      
      ]
 
