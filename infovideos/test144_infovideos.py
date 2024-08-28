4f3eef5598572965a6710588126ad0722281b71c
jaratask2 <>
2024-08-28 16:26:02 -0700

    test144 infovideos file

---
+++ infovideos/test144_infovideos.py
@@ -0,0 +1,114 @@
+from jaratoolbox import videoinfo
+
+subject = 'test144'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+# Session parameters: date, sessionType, videoFile, behavFile, cameraParams
+
+# 2024-07-19
+
+videos.add_session('2024-08-26', "AM",
+                   'test144_am_tuning_curve_20240826_01',
+                   'test144_am_tuning_curve_20240826a', cameraParams) 
+               
+videos.add_session('2024-08-26', "pureTones",
+                   'test143_am_tuning_curve_20240826_02',
+                   'test143_am_tuning_curve_20240826b', cameraParams) 
+                   
+
+videos.add_session('2024-08-26', "naturalSound",
+                   'test143_natural_sound_detection_20240826_01',
+                   'test143_natural_sound_detection_20240826a', cameraParams)                    
+                   
+                  
+videos.add_session('2024-08-26', "AM",
+                   'test144_am_tuning_curve_20240826_03',
+                   'test144_am_tuning_curve_20240826c', cameraParams) 
+               
+videos.add_session('2024-08-26', "pureTones",
+                   'test143_am_tuning_curve_20240826_04',
+                   'test143_am_tuning_curve_20240826d', cameraParams) 
+                                     
+# 2024-08-26 (evening) 
+
+videos.add_session('2024-08-26', "AM",
+                   'test144_am_tuning_curve_20240826_05',
+                   'test144_am_tuning_curve_20240826e', cameraParams) 
+               
+videos.add_session('2024-08-26', "pureTones",
+                   'test143_am_tuning_curve_20240826_06',
+                   'test143_am_tuning_curve_20240826f', cameraParams) 
+                   
+
+videos.add_session('2024-08-26', "naturalSound",
+                   'test143_natural_sound_detection_20240826_02',
+                   'test143_natural_sound_detection_20240826b', cameraParams)                    
+                   
+                  
+videos.add_session('2024-08-26', "AM",
+                   'test144_am_tuning_curve_20240826_07',
+                   'test144_am_tuning_curve_20240826g', cameraParams) 
+               
+videos.add_session('2024-08-26', "pureTones",
+                   'test143_am_tuning_curve_20240826_08',
+                   'test143_am_tuning_curve_20240826h', cameraParams) 
+                   
+videos.add_session('2024-08-26', "naturalSound",
+                   'test143_natural_sound_detection_20240826_03',
+                   'test143_natural_sound_detection_20240826c', cameraParams)                    
+                 
+# 2024-08-27
+
+videos.add_session('2024-08-27', "AM",
+                   'test144_am_tuning_curve_20240827_01',
+                   'test144_am_tuning_curve_20240827a', cameraParams) 
+               
+videos.add_session('2024-08-27', "pureTones",
+                   'test143_am_tuning_curve_20240827_02',
+                   'test143_am_tuning_curve_20240827b', cameraParams) 
+                   
+
+videos.add_session('2024-07-27', "naturalSound",
+                   'test143_natural_sound_detection_20240827_01',
+                   'test143_natural_sound_detection_20240827a', cameraParams)                    
+                   
+                  
+videos.add_session('2024-08-27', "AM",
+                   'test144_am_tuning_curve_20240827_03',
+                   'test144_am_tuning_curve_20240827c', cameraParams) 
+               
+videos.add_session('2024-08-27', "pureTones",
+                   'test143_am_tuning_curve_20240827_04',
+                   'test143_am_tuning_curve_20240827d', cameraParams) 
+                   
+videos.add_session('2024-08-27', "naturalSound",
+                   'test143_natural_sound_detection_20240827_02',
+                   'test143_natural_sound_detection_20240827b', cameraParams)                    
+ # 2024-08-28
+ 
+ videos.add_session('2024-08-28', "AM",
+                   'test144_am_tuning_curve_20240828_01',
+                   'test144_am_tuning_curve_20240828a', cameraParams) 
+               
+videos.add_session('2024-08-28', "pureTones",
+                   'test143_am_tuning_curve_20240828_02',
+                   'test143_am_tuning_curve_20240828b', cameraParams) 
+                   
+
+videos.add_session('2024-07-28', "naturalSound",
+                   'test143_natural_sound_detection_20240828_01',
+                   'test143_natural_sound_detection_20240828a', cameraParams)                    
+                   
+                  
+videos.add_session('2024-08-28', "AM",
+                   'test144_am_tuning_curve_20240828_03',
+                   'test144_am_tuning_curve_20240828c', cameraParams) 
+               
+videos.add_session('2024-08-28', "pureTones",
+                   'test143_am_tuning_curve_20240828_04',
+                   'test143_am_tuning_curve_20240828d', cameraParams) 
+                   
+videos.add_session('2024-08-28', "naturalSound",
+                   'test143_natural_sound_detection_20240828_02',
+                   'test143_natural_sound_detection_20240828b', cameraParams)                                                                                            
