64ae7ab673e996ca03e5a84f454ba78d0b98d8c9
sjara <sjara@uoregon.edu>
2024-09-16 16:59:36 -0700

    Added missing acid tracks files.

---
+++ infohistology/acid009_tracks.py
@@ -0,0 +1,8 @@
+"""
+Tracks information for acid009. Implanted Neuropixels v1.0 in auditory cortex.
+"""
+
+# NOTE: the wiki notes say AC_left, but the histo images show right AC.
+tracks = [{'subject':'acid009', 'brainArea':'AC_left', 'histImage':'p1_B3',
+           'recordingTrack':'implant_DiD', 'shank':1, 'atlasZ': 198}] #tip: p1_c5_R
+
---
+++ infohistology/acid010_tracks.py
@@ -0,0 +1,7 @@
+"""
+Tracks information for acid010. Implanted Neuropixels v1.0 in auditory cortex.
+"""
+
+# NOTE: the wiki notes say AC_left, but the histo images show right AC.
+tracks = [{'subject':'acid010', 'brainArea':'AC_left', 'histImage':'p1_A6',
+           'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 200}] #tip: p1_c5_R
