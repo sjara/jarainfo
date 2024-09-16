80d41d27639a9ffec42d7ee8b16fa44f2dcfeeb8
sjara <sjara@uoregon.edu>
2024-09-16 16:58:03 -0700

    Minor changes to acid006 and acid007 tracks files.

--- infohistology/acid006_tracks.py
+++ infohistology/acid006_tracks.py
@@ -1,3 +1,7 @@
-tracks = [
-	{'subject':'acid006', 'brainArea':'AC_right', 'histImage':'p1_c2_R', 'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 185} #tip: p1_c5_R
-	]
+"""
+Tracks information for acid006. Implanted Neuropixels v1.0 in auditory cortex.
+"""
+
+tracks = [{'subject':'acid006', 'brainArea':'AC_right', 'histImage':'p1_c2_03-04',
+           'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 190}] #tip: p1_c5_R
+
--- infohistology/acid007_tracks.py
+++ infohistology/acid007_tracks.py
@@ -1,3 +1,7 @@
+"""
+Tracks information for acid007. Implanted Neuropixels v1.0 in auditory cortex.
+"""
+
 tracks = [
-	{'subject':'acid007', 'brainArea':'AC_right', 'histImage':'p1_c3_R', 'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 210}
+	{'subject':'acid007', 'brainArea':'AC_right', 'histImage':'p1_c3_03-04', 'recordingTrack':'implant_DiI', 'shank':1, 'atlasZ': 210}
 	]
