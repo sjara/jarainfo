36fbf13608dd29af6fdd2be36e93c9f1593b4482
jarauser <jarauser@jarastation4>
2024-09-16 15:46:54 -0700

    updates

---
+++ infohistology/feat019_tracks.py
@@ -0,0 +1,27 @@
+subject = 'feat019'
+tracks = [
+# Left hemisphere
+
+# {'subject':'feat019', 'brainArea':'xxx_AC', 'histImage':'xx-xx-xx', 'recordingTrack':'xX_DiX', 'shank':1, 'atlasZ':xxx}, 
+
+{'subject':'feat019', 'brainArea':'left_AC', 'histImage':'p1-B6-01', 'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':203}, 
+
+{'subject':'feat019', 'brainArea':'left_AC', 'histImage':'p1-C2-01', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':211}, 
+
+{'subject':'feat019', 'brainArea':'left_AC', 'histImage':'p1-C2-01', 'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':211}, 
+
+{'subject':'feat019', 'brainArea':'left_AC', 'histImage':'p1-C6-01', 'recordingTrack':'anteriorCenter_DiD', 'shank':1, 'atlasZ':229}, 
+
+{'subject':'feat019', 'brainArea':'left_AC', 'histImage':'p1-C6-01', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':229}, 
+
+# Right hemisphere 
+
+{'subject':'feat019', 'brainArea':'right_AC', 'histImage':'p1-C1-02', 'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':206}, 
+
+{'subject':'feat019', 'brainArea':'right_AC', 'histImage':'p1-C2-02', 'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':211}, 
+
+{'subject':'feat019', 'brainArea':'right_AC', 'histImage':'p1-C3-02', 'recordingTrack':'centerCenter_DiO', 'shank':1, 'atlasZ':217}, 
+
+# Note that the DiO and DiI penetrations are right on top of each other
+
+]
