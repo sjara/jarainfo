4ceb2ef3ffb2d87ca7f443a3d35141573d378be6
jarauser <jarauser@jarastation4>
2024-09-17 10:04:13 -0700

    tracks files for test animals used to determine best coordinates for recording from AC

---
+++ infohistology/test140_tracks.py
@@ -0,0 +1,29 @@
+subject = 'test140'
+tracks = [
+    {'subject':'test140', 'brainArea':'AC_left', 'histImage':'p1-B2-01',
+     'recordingTrack':'anteriorMedial_DiD', 'shank':1, 'atlasZ':188},
+     
+     {'subject':'test140', 'brainArea':'AC_left', 'histImage':'p1-A2-01',
+     'recordingTrack':'posteriorLateral_DiD', 'shank':1, 'atlasZ':160},
+     
+      {'subject':'test140', 'brainArea':'AC_left', 'histImage':'p1-A2-01',
+     'recordingTrack':'posteriorMedial_DiI', 'shank':1, 'atlasZ':160},
+     
+     {'subject':'test140', 'brainArea':'AC_left', 'histImage':'p1-B3-01',
+     'recordingTrack':'anteriorLateral_DiI', 'shank':1, 'atlasZ':190}, # left all done in inkscape
+     
+     #right hemisphere
+     
+     {'subject':'test140', 'brainArea':'AC_right', 'histImage':'p1-A5-02',
+     'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':175},
+     
+     {'subject':'test140', 'brainArea':'AC_right', 'histImage':'p1-A4-02',
+     'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':169},
+     
+     {'subject':'test140', 'brainArea':'AC_right', 'histImage':'p1-C3-02',
+     'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':214},
+     
+     {'subject':'test140', 'brainArea':'AC_right', 'histImage':'p1-C2-02',
+     'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':209}, #right all done in inkscape
+     
+     ]
