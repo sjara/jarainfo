55c6c2d2d5a9e82b80b386e277a9c0032c6c1866
jarauser <jarauser@jarastation4>
2024-09-17 10:05:23 -0700

    test animals for AC coordinate determination

---
+++ infohistology/test141_tracks.py
@@ -0,0 +1,29 @@
+subject = 'test141'
+tracks = [
+    {'subject':'test141', 'brainArea':'AC_left', 'histImage':'p1-B5-01', #left hemisphere
+     'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':177},
+     
+     {'subject':'test141', 'brainArea':'AC_left', 'histImage':'p1-C1-01',
+     'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':186},
+     
+      {'subject':'test141', 'brainArea':'AC_left', 'histImage':'p1-D2-01',
+     'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':219},
+     
+     {'subject':'test141', 'brainArea':'AC_left', 'histImage':'p1-D2-01',
+     'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':219}, 
+     
+     #right hemisphere
+     
+     {'subject':'test141', 'brainArea':'AC_right', 'histImage':'p1-B5-02', 
+     'recordingTrack':'posteriorMedial_DiD', 'shank':1, 'atlasZ':177},
+     
+     {'subject':'test141', 'brainArea':'AC_right', 'histImage':'p1-B5-02',
+     'recordingTrack':'posteriorLateral_DiI', 'shank':1, 'atlasZ':177},
+     
+     {'subject':'test141', 'brainArea':'AC_right', 'histImage':'p1-C6-02',
+     'recordingTrack':'anteriorMedial_DiI', 'shank':1, 'atlasZ':213},
+     
+     {'subject':'test141', 'brainArea':'AC_right', 'histImage':'p1-D1-02',
+     'recordingTrack':'anteriorLateral_DiD', 'shank':1, 'atlasZ':217},
+     
+     ]
