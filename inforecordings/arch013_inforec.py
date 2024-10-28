11f1783f33ae2b8d68119c4a0d619a681207d42b
jaratask2 <>
2024-10-28 09:26:07 -0700

    inforec file of arch013

--- inforecordings/arch013_inforec.py
+++ inforecordings/arch013_inforec.py
@@ -7,7 +7,7 @@ subject = 'arch013'
 experiments = []
 
 
-exp0 = celldatabase.Experiment(subject, '2024-10-23', brainArea='left_pStr', probe = 'NPv2-0282', recordingTrack='centerCenter_DiD', info=['facesMedial', 'soundRight'])
+exp0 = celldatabase.Experiment(subject, '2024-10-23', brainArea='left_pStr', probe = 'NPv2-0282', recordingTrack='centerCenter_DiI', info=['facesMedial', 'soundRight'])
 experiments.append(exp0)
 
 exp0.add_site(3780)
@@ -20,25 +20,56 @@ exp0.add_site(3780)
 # 395 trials into it but the open ephys crashed.
 # the ephys crashed  times at "16-31-09".
 
-exp0.add_session('16-50-11','a','optoTuningAM','am_tuning_curve')
-# 403 trials were presented in "16-50-11". 
+exp0.add_session('16-50-11','a','optoTuningAM','am_tuning_curve')  # 403 trials 
+ 
 exp0.add_session('17-08-09','a','optoNaturalCategories','natural_sound_detection')
 
 
 exp0.add_site(4500)
 # Reference = 2:tip
 # Electrode preset = All Shanks 1-96
-exp0.add_session('17-40-44','b','optoTuningAM','am_tuning_curve')
-# 479 trials were presented in "17-40-44". 
-exp0.add_session('17-57-36','b','optoNaturalCategories','am_tuning_curve')
-# went to 42 trials and it crashed
+exp0.add_session('17-40-44','b','optoTuningAM','am_tuning_curve')  # 479 trials 
+
+exp0.add_session('17-57-36','b','optoNaturalCategories','natural_sound_detection')  # went to 42 trials and it crashed
+
 exp0.maxDepth = 4500
 
 # 12:55 animal in the rig
 # 12:58 sylgard off
-#  ground wire in the well
-#  probe in the brain
-#  started recording with shanks 1-96
-#  recording with shanks 97-192
+# 13:02 ground wire in the well
+# 15:22 probe in the brain
+# 16:14 started recording with shanks 97-192
+# 17:40 recording with shanks 1-96
+# 18:02 finished recording
+# 18:16 probe out of brain
+
+exp1 = celldatabase.Experiment(subject, '2024-10-25', brainArea='left_pStr', probe = 'NPv2-0282', recordingTrack='centerCateral_DiD', info=['facesLateral', 'soundRight'])
+experiments.append(exp1)
+
+exp1.add_site(4500)
+# Reference = 2:tip
+# Electrode preset = All Shanks 1-96 
+
+exp1.add_session('15-55-02','a','optoTuningAM','am_tuning_curve')  # went to 460 trials 
+exp1.add_session('16-11-49','a','optoNaturalCategories','natural_sound_detection')
+exp1.add_session('16-42-25','b','optoTuningFreq','am_tuning_curve')
+exp1.add_session('17-00-37','b','optoNaturalInstances','natural_sound_detection')
+
+exp1.add_site(3780)
+# Reference = 2:tip
+# Electrode preset = All Shanks 97-192
+exp1.add_session('17-31-21','c','optoTuningAM','am_tuning_curve')  
+exp1.add_session('17-45-43','c','optoNaturalCategories','natural_sound_detection')
+exp1.add_session('18-13-43','d','optoTuningFreq','am_tuning_curve')
+exp1.add_session('18-30-46','d','optoNaturalInstances','natural_sound_detection')
+
+exp1.maxDepth = 4500
+
+# 13:33 animal in the rig
+# 13:36 sylgard off
+# 13:40 ground wire in the well
+# 15:32 probe in the brain
+# 15:50 started recording with shanks 1-96
+# 17:31 recording with shanks 97-192
 #  finished recording
 #  probe out of brain
