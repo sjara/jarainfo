27f1547783348b3ec0c1a3067cefa5f36befca8f
Jenny Mohn <jlmohn@ucdavis.edu>
2025-02-26 15:18:39 -0800

    test164 infovideos file

--- infovideos/test164_infovideos.py
+++ infovideos/test164_infovideos.py
@@ -6,17 +6,126 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 
 # Session parameters: date, sessionType, videoFile, behavFile, cameraParams
 
-# 2025-02-xx
+ # 2025-02-11
 
-videos.add_session('2025-02-xx', 'pureTones',
-                   'test164_am_tuning_curve_202502xx_01.mkv',
-                   'test164_am_tuning_curve_202502xxa.h5', cameraParams) 
+videos.add_session('2025-02-11', 'pureTones',
+                   'test164_am_tuning_curve_20250211_01.mkv',
+                   'test164_am_tuning_curve_20250211a.h5', cameraParams) 
                    
                    
-videos.add_session('2025-02-xx', 'naturalSounds',
-                   'test164_natural_sound_detection_202502xx_01.mkv',
-                   'test1164_natural_sound_detection_202502xxa.h5', cameraParams) 
+videos.add_session('2025-02-11', 'naturalSounds',
+                   'test164_natural_sound_detection_20250211_01.mkv',
+                   'test1164_natural_sound_detection_20250211a.h5', cameraParams) 
                    
-videos.add_session('2025-02-xx', 'AM',
-                   'test164_am_tuning_curve_202502xx_02.mkv',
-                   'test164_am_tuning_curve_202502xxb.h5', cameraParams) 
+videos.add_session('2025-02-11', 'AM',
+                   'test164_am_tuning_curve_20250211_02.mkv',
+                   'test164_am_tuning_curve_20250211b.h5', cameraParams) 
+                   
+# 2025-02-13
+
+videos.add_session('2025-02-13', 'pureTones',
+                   'test164_am_tuning_curve_20250213_01.mkv',
+                   'test164_am_tuning_curve_20250213a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-13', 'naturalSounds',
+                   'test164_natural_sound_detection_20250213_01.mkv',
+                   'test1164_natural_sound_detection_20250213a.h5', cameraParams) 
+                   
+videos.add_session('2025-02-13', 'AM',
+                   'test164_am_tuning_curve_20250213_02.mkv',
+                   'test164_am_tuning_curve_20250213b.h5', cameraParams) 
+  
+# 2025-02-19
+
+videos.add_session('2025-02-19', 'pureTones',
+                   'test164_am_tuning_curve_20250219_01.mkv',
+                   'test164_am_tuning_curve_20250219a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-19', 'naturalSounds',
+                   'test164_natural_sound_detection_20250219_01.mkv',
+                   'test1164_natural_sound_detection_20250219a.h5', cameraParams) 
+                   
+videos.add_session('2025-02-19', 'AM',
+                   'test164_am_tuning_curve_20250219_02.mkv',
+                   'test164_am_tuning_curve_20250219b.h5', cameraParams) 
+  
+# 4 recordings along each individual shank with only pure tones and AM
+
+# 2025-02-20 - shank 1      
+  
+videos.add_session('2025-02-20', 'pureTones',
+                   'test164_am_tuning_curve_20250220_01.mkv',
+                   'test164_am_tuning_curve_20250220a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-20', 'AM',
+                   'test164_am_tuning_curve_20250219_02.mkv',
+                   'test164_am_tuning_curve_20250219b.h5', cameraParams)   
+                   
+ # 2025-02-20 - shank 2      
+  
+videos.add_session('2025-02-20', 'pureTones',
+                   'test164_am_tuning_curve_20250220_03.mkv',
+                   'test164_am_tuning_curve_20250220c.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-20', 'AM',
+                   'test164_am_tuning_curve_20250220_04.mkv',
+                   'test164_am_tuning_curve_20250220d.h5', cameraParams)  
+                   
+# 2025-02-20 - shank 3     
+  
+videos.add_session('2025-02-20', 'pureTones',
+                   'test164_am_tuning_curve_20250220_05.mkv',
+                   'test164_am_tuning_curve_20250220e.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-20', 'AM',
+                   'test164_am_tuning_curve_20250220_06.mkv',
+                   'test164_am_tuning_curve_20250220f.h5', cameraParams)  
+                   
+ # 2025-02-20 - shank 4      
+  
+videos.add_session('2025-02-20', 'pureTones',
+                   'test164_am_tuning_curve_20250220_07.mkv',
+                   'test164_am_tuning_curve_20250220g.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-20', 'AM',
+                   'test164_am_tuning_curve_20250220_08.mkv',
+                   'test164_am_tuning_curve_20250220h.h5', cameraParams)  
+
+# All shanks active - pure, natural, and AM presented 
+
+# 2025-02-20
+
+videos.add_session('2025-02-20', 'pureTones',
+                   'test164_am_tuning_curve_20250220_09.mkv',
+                   'test164_am_tuning_curve_20250220i.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-20', 'naturalSounds',
+                   'test164_natural_sound_detection_20250220_01.mkv',
+                   'test1164_natural_sound_detection_20250220a.h5', cameraParams) 
+                   
+videos.add_session('2025-02-20', 'AM',
+                   'test164_am_tuning_curve_20250220_10.mkv',
+                   'test164_am_tuning_curve_20250220j.h5', cameraParams) 
+                   
+# 2025-02-21
+
+videos.add_session('2025-02-21', 'pureTones',
+                   'test164_am_tuning_curve_20250221_01.mkv',
+                   'test164_am_tuning_curve_20250221a.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-02-21', 'naturalSounds',
+                   'test164_natural_sound_detection_20250221_01.mkv',
+                   'test1164_natural_sound_detection_20250221a.h5', cameraParams) 
+                   
+videos.add_session('2025-02-21', 'AM',
+                   'test164_am_tuning_curve_20250221_2.mkv',
+                   'test164_am_tuning_curve_20250221b.h5', cameraParams) 
+             
