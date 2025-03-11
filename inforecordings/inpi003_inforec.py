eb3c65b3b3e6cc9048ed6733812c8c57f0aae61f
Jenny Mohn <jlmohn@ucdavis.edu>
2025-03-10 17:25:14 -0700

    20250310_sessions

---
+++ inforecordings/inpi003_inforec.py
@@ -0,0 +1,36 @@
+
+from jaratoolbox import celldatabase
+
+subject = 'inpi003'
+experiments=[]
+
+exp0 = celldatabase.Experiment(subject, '2025-03-10', 'right_AC', 
+                               'centerCenter_DiI','NPv2-3813',
+                               info = ['faceRight','soundLeft'])
+# animal in rig at 15:25
+
+# Shank 1 bank A
+exp0.add_site(4001)
+exp0.add_session('15-30-45', 'a', 'Freq', 'am_tuning_curve', date='2025-03-10')
+exp0.add_session('16-17-01', 'e','AM','am_tuning_curve', date='2025-03-10')
+
+# Shank 2 bank A
+exp0.add_site(4002)
+exp0.add_session('15-43-57', 'b', 'Freq', 'am_tuning_curve', date='2025-03-10')
+exp0.add_session('16-31-36','f','AM','am_tuning_curve', date='2025-03-10')
+
+# Shank 3 bank A
+exp0.add_session('15-55-03', 'c', 'Freq', 'am_tuning_curve', date='2025-03-10')
+exp0.add_session('16-40-22','g','AM','am_tuning_curve', date='2025-03-10')
+
+# Shank 4 bank A
+exp0.add_session('16-06-35', 'd', 'Freq', 'am_tuning_curve', date='2025-03-10')
+exp0.add_session('16-51-00','h','AM','am_tuning_curve', date='2025-03-10')
+
+# animal out of rig at 17:15
+
+experiments.append(exp0)
+
+
+
+
