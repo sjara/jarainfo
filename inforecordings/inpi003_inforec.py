9f35141bf3dbe384662449e217d5441e1273e745
jarauser <jarauser@jararig>
2025-03-12 10:58:08 -0700

    20250310_sessions

--- inforecordings/inpi003_inforec.py
+++ inforecordings/inpi003_inforec.py
@@ -7,25 +7,30 @@ experiments=[]
 exp0 = celldatabase.Experiment(subject, '2025-03-10', 'right_AC', 
                                'centerCenter_DiI','NPv2-3813',
                                info = ['faceRight','soundLeft'])
-# animal in rig at 15:25
+# Animal in rig at 15:25
 
-# Shank 1 bank A
+
+# REMINDER: remove dates from sessions
+
+# Shank 1 bank A  (ADD WHERE REFERENCE IS...)
 exp0.add_site(4001)
-exp0.add_session('15-30-45', 'a', 'Freq', 'am_tuning_curve', date='2025-03-10')
-exp0.add_session('16-17-01', 'e','AM','am_tuning_curve', date='2025-03-10')
+exp0.add_session('15-30-45', 'a', 'Freq', 'am_tuning_curve')
+exp0.add_session('16-17-01', 'e','AM','am_tuning_curve')
 
 # Shank 2 bank A
 exp0.add_site(4002)
-exp0.add_session('15-43-57', 'b', 'Freq', 'am_tuning_curve', date='2025-03-10')
-exp0.add_session('16-31-36','f','AM','am_tuning_curve', date='2025-03-10')
+exp0.add_session('15-43-57', 'b', 'Freq', 'am_tuning_curve')
+exp0.add_session('16-31-36','f','AM','am_tuning_curve')
 
 # Shank 3 bank A
-exp0.add_session('15-55-03', 'c', 'Freq', 'am_tuning_curve', date='2025-03-10')
-exp0.add_session('16-40-22','g','AM','am_tuning_curve', date='2025-03-10')
+exp0.add_site(4003)
+exp0.add_session('15-55-03', 'c', 'Freq', 'am_tuning_curve')
+exp0.add_session('16-40-22','g','AM','am_tuning_curve')
 
 # Shank 4 bank A
-exp0.add_session('16-06-35', 'd', 'Freq', 'am_tuning_curve', date='2025-03-10')
-exp0.add_session('16-51-00','h','AM','am_tuning_curve', date='2025-03-10')
+exp0.add_site(4004)
+exp0.add_session('16-06-35', 'd', 'Freq', 'am_tuning_curve')
+exp0.add_session('16-51-00','h','AM','am_tuning_curve')
 
 # animal out of rig at 17:15
 
