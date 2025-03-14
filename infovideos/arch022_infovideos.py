0fefb67c615c138a73ab425e411ef0cb384a8bf8
jaratask2 <>
2025-03-14 16:02:13 -0700

    Inforvideos for arch022

---
+++ infovideos/arch022_infovideos.py
@@ -0,0 +1,89 @@
+from jaratoolbox import videoinfo
+
+subject = 'arch022'
+videos = videoinfo.Videos(subject)
+cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
+
+# Session parameters: date, sessionType, videoFile, behavFile, cameraParams
+
+# 2025-03-13
+
+videos.add_session('2025-03-13', 'optoTuningAM',
+                   'arch022_am_tuning_curve_20250313_01.mkv',
+                   'arch022_am_tuning_curve_20250313a.h5', cameraParams) 
+                   
+                
+                
+videos.add_session('2025-03-13', 'optoNaturalCategories',
+                   'arch022_natural_sound_detection_20250312_01.mkv',
+                   'arch022_natural_sound_detection_20250313a.h5', cameraParams) 
+                  
+videos.add_session('2025-03-13', 'optoTuningFreq',
+                   'arch022_am_tuning_curve_20250313_02.mkv',
+                   'arch022_am_tuning_curve_20250313b.h5', cameraParams) 
+                   
+                  
+videos.add_session('2025-03-13', 'optoNaturalInstances',
+                   'arch022_natural_sound_detection_20250313_02.mkv',
+                   'arch022_natural_sound_detection_20250313b.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-03-13', 'optoTuningAM',
+                   'arch022_am_tuning_curve_20250313_03.mkv',
+                   'arch022_am_tuning_curve_20250313c.h5', cameraParams) 
+                    
+                   
+videos.add_session('2025-03-13', 'optoNaturalCategories',
+                   'arch022_natural_sound_detection_20250313_03.mkv',
+                   'arch022_natural_sound_detection_20250313c.h5', cameraParams) 
+                    
+videos.add_session('2025-03-13', 'optoTuningFreq',
+                   'arch022_am_tuning_curve_20250313_04.mkv',
+                   'arch022_am_tuning_curve_20250313d.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-03-13', 'optoNaturalInstances',
+                   'arch022_natural_sound_detection_20250313_04.mkv',
+                   'arch022_natural_sound_detection_20250313d.h5', cameraParams) 
+# 2025-03-14
+
+#===================================================================
+
+
+videos.add_session('2025-03-14', 'optoTuningAM',
+                   'arch022_am_tuning_curve_20250314_01.mkv',
+                   'arch022_am_tuning_curve_20250314a.h5', cameraParams) 
+                   
+                
+                
+videos.add_session('2025-03-14', 'optoNaturalCategories',
+                   'arch022_natural_sound_detection_20250314_01.mkv',
+                   'arch022_natural_sound_detection_20250314a.h5', cameraParams) 
+                  
+videos.add_session('2025-03-14', 'optoTuningFreq',
+                   'arch022_am_tuning_curve_20250314_02.mkv',
+                   'arch022_am_tuning_curve_20250314b.h5', cameraParams) 
+                   
+                  
+videos.add_session('2025-03-14', 'optoNaturalInstances',
+                   'arch022_natural_sound_detection_20250314_02.mkv',
+                   'arch022_natural_sound_detection_20250314b.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-03-14', 'optoTuningAM',
+                   'arch022_am_tuning_curve_20250314_03.mkv',
+                   'arch022_am_tuning_curve_20250314c.h5', cameraParams) 
+                    
+                   
+videos.add_session('2025-03-14', 'optoNaturalCategories',
+                   'arch022_natural_sound_detection_20250314_03.mkv',
+                   'arch022_natural_sound_detection_20250314c.h5', cameraParams) 
+                    
+videos.add_session('2025-03-14', 'optoTuningFreq',
+                   'arch022_am_tuning_curve_20250314_04.mkv',
+                   'arch022_am_tuning_curve_20250314d.h5', cameraParams) 
+                   
+                   
+videos.add_session('2025-03-14', 'optoNaturalInstances',
+                   'arch022_natural_sound_detection_20250314_04.mkv',
+                   'arch022_natural_sound_detection_20250314d.h5',
