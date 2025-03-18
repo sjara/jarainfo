5e0a076847e6343a961fd3a25b80380a0c8274a8
jarauser <jarauser@jararig>
2025-03-18 10:47:17 -0700

    20250317 recordings

--- inforecordings/inpi003_inforec.py
+++ inforecordings/inpi003_inforec.py
@@ -42,7 +42,7 @@ exp1 = celldatabase.Experiment(subject, '2025-03-17', 'right_AC',
 
 # mouse in rig1 16:15
 
-exp1.add_site(2000)
+exp1.add_site(2560)
 exp1.add_session('16-18-44','a','Freq','am_tuning_curve')
 exp1.add_session('16-28-03','b','AM','am_tuning_curve')
 exp1.add_session('16-35-57','a','naturalSound','natural_souund_detection')
