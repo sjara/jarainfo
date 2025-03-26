c56e447948a17fbc62c10266317fc17ec67c009f
jaratask2 <>
2025-03-26 09:22:11 -0700

    inforec for arch022

--- inforecordings/arch020_inforec.py
+++ inforecordings/arch020_inforec.py
@@ -33,9 +33,35 @@ exp0.maxDepth = 3500
 # probe in brain around 13:00 pm
 
 # exp 0 finish
+# =================================2025-03-25============================================
+# exp 1 start.
+# shanks placed parallel to bregma-lambda axis, and around same position as yesterday.
+exp1 = celldatabase.Experiment(subject, '2025-03-25', brainArea='right_pStr', probe = 'NPv2-1134', recordingTrack='centerCenter_DiI', info=['facesMedial', 'soundLeft'])
+experiments.append(exp1)
 
+exp1.add_site(3500)
 
+# Reference = 1:tip
+# Electrode preset = All Shanks 1-96 
 
+exp1.add_session('12-48-53','a','optoTuningAM','am_tuning_curve')  
+#recorded 506 trials instead of 440.
+exp1.add_session('13-56-00','a','optoNaturalCategories','natural_sound_detection')
+# Its video behavior file got substituted with next one by mistake-vdofile recorded at  13:47, so recording this optonautralCategories again. first file for this is 13-05-22, and second is 13-56-00.
+# The paradigm file(-extra) and ephy recoding is available for both 13-05-22 and 13-56-00.
+exp1.add_session('13-32-36','b','optoTuningFreq','am_tuning_curve')
+exp1.add_session('14-23-42','b','optoNaturalInstances','natural_sound_detection')
+
+exp1.add_site(2780)
+# reference = 1:tip
+# Electrode preset = All Shanks 97-192
+exp1.add_session('14-48-56','c','optoTuningAM','am_tuning_curve') 
+exp1.add_session('15-03-05','c','optoNaturalCategories','natural_sound_detection')
+exp1.add_session('15-32-03','d','optoTuningFreq','am_tuning_curve')
+exp1.add_session('15-58-18','d','optoNaturalInstances','natural_sound_detection')
+
+exp1.maxDepth = 3500
+# exp1 finish
 #control exp start
 
 
