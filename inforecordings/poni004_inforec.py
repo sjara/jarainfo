b3d1dca5aa61e9f1178eeb4b1fc818de702e3141
ramzymulla <rza@uoregon.edu>
2025-08-15 17:30:24 -0700

    20250815 session

--- inforecordings/poni004_inforec.py
+++ inforecordings/poni004_inforec.py
@@ -111,43 +111,79 @@ exp4.maxDepth=3004  # Xpia=13200, Xfinal=16200
 
 exp4.add_site(3001) # shank 1 bank A, tip 2
 exp4.add_session('13-41-10','e','tuningFreq','am_tuning_curve') 
-exp4.add_session('14-38-14','j','optoAM','am_tuning_curve')  # laser set to 9.76 (10 mW)
+exp4.add_session('14-38-14','j','optoAM_10mW','am_tuning_curve')  # laser set to 9.76 (10 mW)
 
-exp4.add_site(3002) # shank 2 bank A, tip2
-exp4.add_session('14-24-59','i','optoAM','am_tuning_curve') # laser set to 9.76 (10 mW)
-
-
-exp4.add_site(3003) # shank 3 bank A, tip 2
-exp4.add_session('14-11-03','h','optoAM','am_tuning_curve') # laser set to 9.76 (10 mW)
+# exp4.add_site(3002) # shank 2 bank A, tip2
+# exp4.add_session('14-24-59','i','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
 
 
-exp4.add_site(3004) # shank 4 bank A, tip 2
-exp4.add_session('13-47-57','f','tuningFreq','am_tuning_curve')
-exp4.add_session('13-57-39','g','optoAM','am_tuning_curve') # laser set to 9.76 (10 mW)
+# exp4.add_site(3003) # shank 3 bank A, tip 2
+# exp4.add_session('14-11-03','h','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
+
+
+# exp4.add_site(3004) # shank 4 bank A, tip 2
+# exp4.add_session('13-47-57','f','tuningFreq','am_tuning_curve')
+# exp4.add_session('13-57-39','g','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
 
 
 exp4.add_site(1564) # 193-288, tip 2
 exp4.add_session('14-51-35','k','tuningFreq','am_tuning_curve')
-exp4.add_session('14-57-18','l','optoAM','am_tuning_curve') # laser set to 9.76 (10 mW)
-exp4.add_session('15-14-04','m','optoAM','am_tuning_curve') # laser set to 4.31 (5 mW)
+exp4.add_session('14-57-18','l','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
+exp4.add_session('15-14-04','m','optoAM_5mW','am_tuning_curve') # laser set to 4.31 (5 mW)
 
 
-exp4.add_site(2284) 
-exp4.add_session('15-28-18','n','tuningFreq','am_tuning_curve') 
-exp4.add_session('15-34-54','o','optoAM','am_tuning_curve') # laser set to 9.76 (10 mW)
-exp4.add_session('15-49-16','p','optoAM','am_tuning_curve') # laser set to 4.31 (5 mW)
+# exp4.add_site(2284) 
+# exp4.add_session('15-28-18','n','tuningFreq','am_tuning_curve') 
+# exp4.add_session('15-34-54','o','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
+# exp4.add_session('15-49-16','p','optoAM_5mW','am_tuning_curve') # laser set to 4.31 (5 mW)
 
-exp4.add_site(121)
-exp4.add_session('16-04-30','q','tuningFreq','am_tuning_curve')
+# exp4.add_site(121)
+# exp4.add_session('16-04-30','q','tuningFreq','am_tuning_curve') # for ID of pia on shank 1
 
-exp4.add_site(124)
-exp4.add_session('16-05-29','r','tuningFreq','am_tuning_curve')
+# exp4.add_site(124)
+# exp4.add_session('16-05-29','r','tuningFreq','am_tuning_curve') # for ID of pia on shank 4
 
-experiments.append(exp3)
+# experiments.append(exp2)
+# experiments.append(exp3)
 experiments.append(exp4)
 ### mouse out 1632
 
 
 
+### 2025-08-15 Session
+
+# mouse in 1300
 
 
+exp5 = celldatabase.Experiment(subject, '2025-08-14', 'left_AC', 
+                               'centerCenter_DiI',probe='NPv2-3082',
+                               info = ['faceFront','soundBilateral'])
+
+exp5.maxDepth=3014    #Xpia=14800, Xf = 17800
+
+exp5.add_site(3014) # shank 4 bank A, tip 4
+exp5.add_session('14-25-40','a','tuningFreq','am_tuning_curve')
+exp5.add_session('15-19-58','e','optoAM_2mW','am_tuning_curve') # laser set to 1.70 (2 mW)
+exp5.add_session('17-13-55','n','optoAM_5mW','am_tuning_curve') # laser set to 4.31 (5 mW)
+exp5.add_session('17-26-45','o','tuningFreq_end','am_tuning_curve')
+
+exp5.add_site(3013) # shank 3 bank A, tip 4
+exp5.add_session('16-51-38','l','tuningFreq','am_tuning_curve')
+exp5.add_session('16-59-17','m','optoAM_2mW','am_tuning_curve') # laser set to 1.70 (2 mW)
+
+exp5.add_site(2290) # 97-192, tip 4
+exp5.add_session('14-42-30','b','tuningFreq','am_tuning_curve')
+exp5.add_session('14-52-25','c','optoAM_2mW','am_tuning_curve') # laser set to 1.70 (2 mW)
+exp5.add_session('15-37-47','f','optoAM_5mW','am_tuning_curve') # laser set to 4.31 (5 mW)
+exp5.add_session('16-38-23','k','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
+
+exp5.add_site(1570) # 193-288, tip 4
+exp5.add_session('16-30-04','j','tuningFreq','am_tuning_curve')
+exp5.add_session('15-06-34','d','optoAM_2mW','am_tuning_curve') # laser set to 1.70 (2 mW)
+exp5.add_session('15-51-25','g','optoAM_5mW','am_tuning_curve') # laser set to 4.31 (5 mW)
+exp5.add_session('16-05-58','h','optoAM_1mW','am_tuning_curve') # laser set to 1.04 (1 mW)
+exp5.add_session('16-14-23','i','optoAM_10mW','am_tuning_curve') # laser set to 9.76 (10 mW)
+
+experiments.append(exp5)
+
+# mouse out 1740
