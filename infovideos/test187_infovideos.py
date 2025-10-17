3d7183359a5f836360061a20abc9e7d8c0cdd832
Lizethgv20 <108704795+Lizethgv20@users.noreply.github.com>
2025-10-17 12:17:31 -0700

    Update session types in test187_infovideos.py

--- infovideos/test187_infovideos.py
+++ infovideos/test187_infovideos.py
@@ -8,7 +8,7 @@ cameraParams = videoinfo.cameraParams['IR_webcam_640x480_30fps_VP9']
 
  # 2025-10-07
 #This is the session with 140 trials
-videos.add_session('2025-10-07', 'pureTones',
+videos.add_session('2025-10-07', 'optoTuningFreq',
                    'test187_am_tuning_curve_20251007_01.mkv',
                    'test187_am_tuning_curve_20251007a.h5', cameraParams) 
                    
@@ -18,11 +18,11 @@ videos.add_session('2025-10-07', 'pureTones',
 #                   'wifi008_natural_sound_detection_20250528.h5', cameraParams)
 
 #session with 320 trials
-videos.add_session('2025-10-07', 'pureTones',
+videos.add_session('2025-10-07', 'optoTuningFreq',
                    'test187_am_tuning_curve_20251007_03.mkv',
                    'test187_am_tuning_curve_20251007c.h5', cameraParams)
 
 #session with 220 trials
-videos.add_session('2025-10-07', 'AM',
+videos.add_session('2025-10-07', 'optoTuningAM',
                    'test187_am_tuning_curve_20251007_04.mkv',
                    'test187_am_tuning_curve_20251007d.h5', cameraParams) 
