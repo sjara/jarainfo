d61bd521641e8a609861d57f1ffaab5ff755d291
jaratask2 <>
2025-11-12 14:08:53 -0800

    Info video updated for arch034

--- infovideos/arch034_infovideos.py
+++ infovideos/arch034_infovideos.py
@@ -27,3 +27,25 @@ videos.add_session('2025-11-10', 'optoTuningFreq',
 videos.add_session('2025-11-10', 'optoNaturalInstances',
                    'arch034_natural_sound_detection_20251110_04.mkv',
                    'arch034_natural_sound_detection_20251110b.h5', cameraParams)
+
+# 2025-11-12
+#session with 461  trials
+videos.add_session('2025-11-12', 'optoTuningAM',
+                   'arch034_am_tuning_curve_20251112_01.mkv',
+                   'arch034_am_tuning_curve_20251112a.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-11-12', 'optoNaturalCategories',
+                   'arch034_natural_sound_detection_20251112_02.mkv',
+                   'arch034_natural_sound_detection_20251112a.h5', cameraParams)
+        
+ 
+#session with 640 trials
+videos.add_session('2025-11-12', 'optoTuningFreq',
+                   'arch034_am_tuning_curve_20251112_03.mkv',
+                   'arch034_am_tuning_curve_20251112b.h5', cameraParams)
+
+#session with 160 trials
+videos.add_session('2025-11-12', 'optoNaturalInstances',
+                   'arch034_natural_sound_detection_20251112_04.mkv',
+                   'arch034_natural_sound_detection_20251112b.h5', cameraParams)
