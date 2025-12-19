e0ddc91c32c3deddcce746e45ffada928e7558b4
jaratask2 <>
2025-12-18 16:33:03 -0800

    Info video updated for arch043

--- infovideos/arch043_infovideos.py
+++ infovideos/arch043_infovideos.py
@@ -27,3 +27,47 @@ videos.add_session('2025-12-15', 'optoTuningFreq',
 videos.add_session('2025-12-15', 'optoNaturalInstances',
                    'arch043_natural_sound_detection_20251215_04.mkv',
                    'arch043_natural_sound_detection_20251215b.h5', cameraParams)
+
+# 2025-12-18
+#session with 440  trials
+videos.add_session('2025-12-18', 'optoTuningAM',
+                   'arch043_am_tuning_curve_20251218_01.mkv',
+                   'arch043_am_tuning_curve_20251218a.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-18', 'optoNaturalCategories',
+                   'arch043_natural_sound_detection_20251218_02.mkv',
+                   'arch043_natural_sound_detection_20251218a.h5', cameraParams)
+        
+ 
+#session with 640 trials
+videos.add_session('2025-12-18', 'optoTuningFreq',
+                   'arch043_am_tuning_curve_20251218_03.mkv',
+                   'arch043_am_tuning_curve_20251218b.h5', cameraParams)
+
+#session with 160 trials
+videos.add_session('2025-12-18', 'optoNaturalInstances',
+                   'arch043_natural_sound_detection_20251218_04.mkv',
+                   'arch043_natural_sound_detection_20251218b.h5', cameraParams)
+
+#Sessions with no laser going in the brain
+#session with 440  trials
+videos.add_session('2025-12-18', 'optoTuningAM',
+                   'arch043_am_tuning_curve_20251218_05.mkv',
+                   'arch043_am_tuning_curve_20251218g.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-18', 'optoNaturalCategories',
+                   'arch043_natural_sound_detection_20251218_06.mkv',
+                   'arch043_natural_sound_detection_20251218c.h5', cameraParams)
+        
+ 
+#session with 640 trials
+videos.add_session('2025-12-18', 'optoTuningFreq',
+                   'arch043_am_tuning_curve_20251218_07.mkv',
+                   'arch043_am_tuning_curve_20251218h.h5', cameraParams)
+
+#session with 160 trials
+videos.add_session('2025-12-18', 'optoNaturalInstances',
+                   'arch043_natural_sound_detection_20251218_08.mkv',
+                   'arch043_natural_sound_detection_20251218d.h5', cameraParams)
