801a7743e1484cd7f5d57ce8ed0fbb7d9d14bab8
jaratask2 <>
2025-12-30 15:04:47 -0800

    Info video updated for arch044

--- infovideos/arch044_infovideos.py
+++ infovideos/arch044_infovideos.py
@@ -21,3 +21,96 @@ videos.add_session('2025-12-28', 'optoNaturalInstances',
                    'arch044_natural_sound_detection_20251228_04.mkv',
                    'arch044_natural_sound_detection_20251228b.h5', cameraParams)
 
+
+# 2025-12-29
+#session with 440  trials
+videos.add_session('2025-12-29', 'optoTuningAM',
+                   'arch044_am_tuning_curve_20251229_01.mkv',
+                   'arch044_am_tuning_curve_20251229a.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2025-12-29', 'optoTuningFreq',
+                   'arch044_am_tuning_curve_20251229_02.mkv',
+                   'arch044_am_tuning_curve_20251229b.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-28', 'optoNaturalCategories',
+                   'arch044_natural_sound_detection_20251229_03.mkv',
+                   'arch044_natural_sound_detection_20251229a.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2025-12-29', 'optoNaturalInstances',
+                   'arch044_natural_sound_detection_20251229_04.mkv',
+                   'arch044_natural_sound_detection_20251229b.h5', cameraParams)
+
+# 2025-12-29 with the laser shining outside 
+#session with 440  trials
+videos.add_session('2025-12-28', 'optoTuningAM',
+                   'arch044_am_tuning_curve_2025122805.mkv',
+                   'arch044_am_tuning_curve_20251228c.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2025-12-28', 'optoTuningFreq',
+                   'arch044_am_tuning_curve_20251228_06.mkv',
+                   'arch044_am_tuning_curve_20251228d.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-28', 'optoNaturalCategories',
+                   'arch044_natural_sound_detection_20251228_07.mkv',
+                   'arch044_natural_sound_detection_20251228c.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2025-12-28', 'optoNaturalInstances',
+                   'arch044_natural_sound_detection_20251228_08.mkv',
+                   'arch044_natural_sound_detection_20251228d.h5', cameraParams)
+
+# 2025-12-30
+#session with 440  trials
+videos.add_session('2025-12-30', 'optoTuningAM',
+                   'arch044_am_tuning_curve_20251230_01.mkv',
+                   'arch044_am_tuning_curve_20251230a.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2025-12-30', 'optoTuningFreq',
+                   'arch044_am_tuning_curve_20251230_02.mkv',
+                   'arch044_am_tuning_curve_20251230b.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-30', 'optoNaturalCategories',
+                   'arch044_natural_sound_detection_20251230_03.mkv',
+                   'arch044_natural_sound_detection_20251230a.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2025-12-30', 'optoNaturalInstances',
+                   'arch044_natural_sound_detection_20251230_04.mkv',
+                   'arch044_natural_sound_detection_20251230b.h5', cameraParams)
+
+
+#These sessions are recorded with the laser shining outside of the brain
+#session with 440  trials
+videos.add_session('2025-12-30', 'optoTuningAM',
+                   'arch044_am_tuning_curve_20251230_05.mkv',
+                   'arch044_am_tuning_curve_20251230c.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2025-12-30', 'optoTuningFreq',
+                   'arch044_am_tuning_curve_20251230_06.mkv',
+                   'arch044_am_tuning_curve_20251230d.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2025-12-30', 'optoNaturalCategories',
+                   'arch044_natural_sound_detection_20251230_07.mkv',
+                   'arch044_natural_sound_detection_20251230c.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2025-12-30', 'optoNaturalInstances',
+                   'arch044_natural_sound_detection_20251230_08.mkv',
+                   'arch044_natural_sound_detection_20251230d.h5', cameraParams)
