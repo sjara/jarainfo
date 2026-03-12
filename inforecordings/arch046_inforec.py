3f258c09d478e708832fbc5c05c6b241678954a9
srezazadeh98 <srez@uoregon.edu>
2026-03-12 11:21:30 -0700

    Add new sites and sessions for experiment data
    Added new sites for data analysis and updated sessions.

--- inforecordings/arch046_inforec.py
+++ inforecordings/arch046_inforec.py
@@ -20,6 +20,7 @@ experiments = []
 #expx.add_session('xx-xx-xx', 'a', 'naturalSound', 'natural_sound_detection')
 #expx.add_session('xx-xx-xx', 'b', 'AM', 'am_tuning_curve')
 
+#.........................................................................................
 
 exp0 = celldatabase.Experiment(subject, '2026-03-03', brainArea='right_pStr', probe='NPv2-3973', recordingTrack='centerCenter_DiI', info=['facesLateral', 'soundLeft'])
 
@@ -36,7 +37,6 @@ experiments.append(exp0)
 exp0.add_site(4000) 
 exp0.maxDepth = 4000
 
-
 # Shank 1 Bank A
 exp0.add_session('13-59-15', 'i', 'tuningFreq', 'am_tuning_curve') #40 trials
 
@@ -49,21 +49,27 @@ exp0.add_session('11-20-38', 'b', 'optoTuningFreq', 'am_tuning_curve') #640 tria
 exp0.add_session('11-42-18', 'a', 'optoNaturalCategories', 'natural_sound_detection') #200 OptoNaturalCategories
 exp0.add_session('12-12-05', 'b', 'optoNaturalInstances', 'natural_sound_detection') #160 OptoNaturalInstances
 
+exp0.add_site(3280)
 #Recoeding from all shanks, channels 97-192.
 exp0.add_session('12-35-48', 'c', 'optoTuningAM', 'am_tuning_curve') #440
 exp0.add_session('12-51-05', 'd', 'optoTuningFreq', 'am_tuning_curve') #640 trials
 exp0.add_session('13-07-15', 'c', 'optoNaturalCategories', 'natural_sound_detection') #212 OptoNaturalCategories
 exp0.add_session('13-34-36', 'd', 'optoNaturalInstances', 'natural_sound_detection') #160 OptoNaturalInstances
 
+exp0.add_site(3999) 
+# IT IS THE SAME SITE AS THE FIRST ONE (4000). I JUST ADDED A NEW SITE FOR DATA ANALYSIS PURPOSES
 #Recording from all shanks, channels 1-9 with the laser shining outside of the brain. 
 exp0.add_session('14-11-14', 'e', 'optoTuningAM', 'am_tuning_curve') #440
 exp0.add_session('14-28-02', 'f', 'optoTuningFreq', 'am_tuning_curve') #640 trials
 
+exp0.add_site(3279)
+# IT IS THE SAME SITE AS THE SECOND ONE (3280). I JUST ADDED A NEW SITE FOR DATA ANALYSIS PURPOSES
 #Recording from all shanks, channels 97-192, with the laser shining outside of the brain. 
 exp0.add_session('14-47-41', 'g', 'optoTuningAM', 'am_tuning_curve') #440
 exp0.add_session('15-02-02', 'h', 'optoTuningFreq', 'am_tuning_curve') #640 trials
 
 
+#...............................................................................................
 
 #2026-03-08
 #exp1 = celldatabase.Experiment(subject, '2026-03-08', brainArea='right_pStr', probe='NPv2-3813', recordingTrack='centerCenter_DiD', info=['facesLateral', 'soundLeft'])
@@ -74,3 +80,5 @@ exp0.add_session('15-02-02', 'h', 'optoTuningFreq', 'am_tuning_curve') #640 tria
 #The eye looks good when put the mouse in the rig.
 #Pwnwtrated at 11:58am. 
 #I was not able to record today because I think the probe could not detect the head stage. I could see the probe being recognized in open ephys but i could not see any signals form any shanks, banks or channels. 
+
+#................................................................................................
