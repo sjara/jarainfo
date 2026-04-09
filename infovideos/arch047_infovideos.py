4f63c05d63cb9ed287bd9c8510799f63f54825c2
jaratask2 <>
2026-04-09 13:50:27 -0700

    info file for arch047

--- infovideos/arch047_infovideos.py
+++ infovideos/arch047_infovideos.py
@@ -68,3 +68,53 @@ videos.add_session('2026-03-31', 'optoTuningAM',
 videos.add_session('2026-03-31', 'optoTuningFreq',
                    'arch047_am_tuning_curve_20260331_12.mkv',
                    'arch047_am_tuning_curve_20260331h.h5', cameraParams)
+
+#....................................................................................................
+# 2026-04-09
+#session with 440  trial, recording from all shanks 1-96.
+videos.add_session('2026-04-09', 'optoTuningAM',
+                   'arch047_am_tuning_curve_20260409_01.mkv',
+                   'arch047_am_tuning_curve_20260409a.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2026-04-09', 'optoTuningFreq',
+                   'arch047_am_tuning_curve_20260409_02.mkv',
+                   'arch047_am_tuning_curve_202604-09b.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2026-04-09', 'optoNaturalCategories',
+                   'arch047_natural_sound_detection_20260409_03.mkv',
+                   'arch047_natural_sound_detection_20260409a.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2026-04-09', 'optoNaturalInstances',
+                   'arch047_natural_sound_detection_20260409_04.mkv',
+                   'arch047_natural_sound_detection_20260409b.h5', cameraParams)
+
+
+# 2026-04-09
+#session with 440  trial, recording from all shanks 97-192.
+videos.add_session('2026-04-09', 'optoTuningAM',
+                   'arch047_am_tuning_curve_20260409_05.mkv',
+                   'arch047_am_tuning_curve_202604-09c.h5', cameraParams)
+
+ 
+#session with 640 trials
+videos.add_session('2026-04-09', 'optoTuningFreq',
+                   'arch047_am_tuning_curve_20260409_06.mkv',
+                   'arch047_am_tuning_curve_20260409d.h5', cameraParams)
+
+#session with 200 trials
+videos.add_session('2026-04-09', 'optoNaturalCategories',
+                   'arch047_natural_sound_detection_20260409_07.mkv',
+                   'arch047_natural_sound_detection_20260409c.h5', cameraParams)
+        
+
+#session with 160 trials
+videos.add_session('2026-04-09', 'optoNaturalInstances',
+                   'arch047_natural_sound_detection_20260409_08.mkv',
+                   'arch047_natural_sound_detection_20260409d.h5', cameraParams)
+
+
