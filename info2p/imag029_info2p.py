cbfc9d401571c553951bd5be317baf99ba70c78a
Evan Vickers <evicker2@uoregon.edu>
2026-04-16 13:46:48 -0700

    corrected 20260402 sessions params for imag029

--- info2p/imag029_info2p.py
+++ info2p/imag029_info2p.py
@@ -80,38 +80,62 @@ newSession = {'subject':subject, 'date':'20260227', 'session': '005',
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '000',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 9.96, 'magnification': 1.0, 'depth': 200, 'angle': 42.93,
+              'laserPower': 65, 'wavelength': 920, 'nFrames': 2000, 
+              'sessionLabel': 'green_lowMag_unidir_test', 'paradigm':'none'}
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '001',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 9.96, 'magnification': 4.0, 'depth': 200, 'angle': 42.93,
+              'laserPower': 65, 'wavelength': 920, 'nFrames': 2000, 
+              'sessionLabel': 'green_highMag_unidir_test', 'paradigm':'none'}
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '002',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 19.92, 'magnification': 1.0, 'depth': 200, 'angle': 42.93,
+              'laserPower': 65, 'wavelength': 920, 'nFrames': 4000, 
+              'sessionLabel': 'green_lowMag_bidir_test', 'paradigm':'none'}
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '003',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 9.96, 'magnification': 1.0, 'depth': 200, 'angle': 42.93,
+              'laserPower': 65, 'wavelength': 920, 'nFrames': 4000, 
+              'sessionLabel': 'green_lowMag_unidir_test', 'paradigm':'none'}
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '004',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 19.92, 'magnification': 1.0, 'depth': 500, 'angle': 43.3,
+              'laserPower': 65, 'wavelength': 920, 'nFrames': 18000, 
+              'sessionLabel': 'green_L5_spont_test_long', 'paradigm':'none'}
 sessions.append(newSession)
 
 newSession = {'subject':subject, 'date':'20260402', 'session': '005',
-              'fps': 15.49, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
-              'laserPower': 65, 'wavelength': 1040, 'nFrames': 250, 
-              'sessionLabel': 'red_anat_zStack', 'paradigm':'none'}
+              'fps': 19.92, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
+              'laserPower': 43, 'wavelength': 1040, 'nFrames': 4000, 
+              'sessionLabel': 'red_zStack_center_single_frame', 'paradigm':'none'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260402', 'session': '006',
+              'fps': 9.96, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
+              'laserPower': 43, 'wavelength': 1040, 'nFrames': 2000, 
+              'sessionLabel': 'red_zStack_-80_to_80_step_10.7', 'paradigm':'none'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260402', 'session': '007',
+              'fps': 9.96, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
+              'laserPower': 28, 'wavelength': 920, 'nFrames': 2000, 
+              'sessionLabel': 'green_zStack_-80_to_80_step_10.7', 'paradigm':'none'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260402', 'session': '008',
+              'fps': 9.96, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
+              'laserPower': 39, 'wavelength': 920, 'nFrames': 3000, 
+              'sessionLabel': 'green_zStack_center_single_frame', 'paradigm':'none'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260402', 'session': '009',
+              'fps': 9.96, 'magnification': 1.0, 'depth': 500, 'angle': 42.93,
+              'laserPower': 43, 'wavelength': 920, 'nFrames': 3000, 
+              'sessionLabel': 'green_zStack_center_single_frame_save_PMT0_and_1', 'paradigm':'none'}
 sessions.append(newSession)
 
