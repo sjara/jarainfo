ae2ddfe791428a1b6e46e293db30bfc13df56934
Evan Vickers <evicker2@uoregon.edu>
2026-07-16 13:03:41 -0700

    new recs

---
+++ info2p/imag039_info2p.py
@@ -0,0 +1,35 @@
+"""
+Metadata for two-photon imaging session
+
+Each new session should include the following paramters:
+subject: in standard jaralab format.
+date: YYYYMMDD
+session: 3-digit number incremented by Scanbox (as string)
+fps: frame rate shown in Scanbox.
+magnification: shown in Scanbox.
+depth: in microns w.r.t. brain surface (calculated from knobby numbers)
+angle: actual objective angle set (even if later it was zeroed).
+laserPower: percentage shown by Scanbox.
+wavelength: laser wavelength.
+nFrames: total number of frames collected.
+sessionLabel: arbitrary name for the type of session you recorded.
+paradigm: name of taskontrol paradigm used during the session.
+
+NOTE: when you save the stimulus/behavior data via the taskontrol paradigm
+      make sure you name the file with the format: SUBJECT_PARADIGM_DATE_SESSION.h5
+      For example: test000_am_tuning_20260401_001.h5
+"""
+
+
+subject = 'imag039'
+sessions = []
+
+newSession = {'subject':subject, 'date':'20260709', 'session': '000',
+              'fps': 9.96, 'magnification': 1.7, 'depth': 180.35, 'angle': 37.52,
+              'laserPower': 31, 'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A1_highFreq', 'sessionLabel': 'tuningFreq', 
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#320 trials
+
+
