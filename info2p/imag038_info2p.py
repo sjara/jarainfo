f5b01df27727fd612b9e3474e66ba8173851fbcc
Evan Vickers <evicker2@uoregon.edu>
2026-07-16 17:20:03 -0700

    imag038 2p 20260716

--- info2p/imag038_info2p.py
+++ info2p/imag038_info2p.py
@@ -24,12 +24,64 @@ NOTE: when you save the stimulus/behavior data via the taskontrol paradigm
 subject = 'imag038'
 sessions = []
 
-newSession = {'subject':subject, 'date':'20260709', 'session': '000',
-              'fps': 9.96, 'magnification': 1.7, 'depth': 180.35, 'angle': 37.52,
-              'laserPower': 31, 'wavelength': 920, 'nFrames': 6115,
-              'brainArea': 'A1_highFreq', 'sessionLabel': 'tuningFreq', 
+newSession = {'subject':subject, 'date':'20260716', 'session': '000',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 138.08, 'angle': 42.57,
+              'laserPower': 45, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A1_medFreq', 'sessionLabel': 'AM_fading', 
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '001',
+              'fps': 9.96, 'magnification': 3.4, 'depth': 215.85, 'angle': 42.57,
+              'laserPower': 54, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A1_medFreq', 'sessionLabel': 'AM_fading', 
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '002',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 152.36, 'angle': 42.57,
+              'laserPower': 52, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A2_medFreq', 'sessionLabel': 'AM_fading', 
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '003',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 151.66, 'angle': 42.57,
+              'laserPower': 47, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'AM_fading', 
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '004',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 151.66, 'angle': 42.57,
+              'laserPower': 47, 'wavelength': 920, 'nFrames': 16000,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'natSounds', 
+              'pmt': [0,1], 'paradigm':'natural_sound_detection'}
+sessions.append(newSession)
+#200 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '005',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 151.66, 'angle': 42.57,
+              'laserPower': 47, 'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningFreq', 
               'pmt': [0,1], 'paradigm':'am_tuning'}
 sessions.append(newSession)
 #320 trials
+
+newSession = {'subject':subject, 'date':'20260716', 'session': '006',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 151.66, 'angle': 42.57,
+              'laserPower': 47, 'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningAM', 
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#320 trials
+
+
+
+
 
 
