21949bb14d8756ee40a90be7bae053ce965fd98a
Evan Vickers <evicker2@uoregon.edu>
2026-07-30 16:37:50 -0700

    imag039 20260730

--- info2p/imag039_info2p.py
+++ info2p/imag039_info2p.py
@@ -331,6 +331,249 @@ newSession = {'subject':subject, 'date':'20260730','session': '000',
 sessions.append(newSession)
 #320 trials
 
+newSession = {'subject':subject, 'date':'20260730','session': '001',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 181.06, 'angle': 42.00,
+              'laserPower': 26,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A1_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '002',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 181.06, 'angle':42.00,
+              'laserPower': 26,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A1_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#220 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '003',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 415.51,'angle': 42.02,
+              'laserPower': 70, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A1_lowFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '004',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 415.51, 'angle': 42.00,
+              'laserPower': 70,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A1_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '005',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 415.51, 'angle':42.00,
+              'laserPower': 70,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A1_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#220 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '006',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 119.25,'angle': 42.02,
+              'laserPower': 26, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '007',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 119.25, 'angle': 42.00,
+              'laserPower': 26,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '008',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 119.25, 'angle':42.00,
+              'laserPower': 26,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+#220 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '009',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 429.39,'angle': 42.02,
+              'laserPower': 81, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '010',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 429.39, 'angle': 42.00,
+              'laserPower': 81,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '011',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 429.39, 'angle': 42.00,
+              'laserPower': 81,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A2_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '012',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 155.85,'angle': 42.02,
+              'laserPower': 23, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '013',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 155.85, 'angle': 42.02,
+              'laserPower': 23,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '014',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 155.85, 'angle': 42.02,
+              'laserPower': 23,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '015',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 475.76,'angle': 42.02,
+              'laserPower': 73, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '016',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 475.76, 'angle': 42.02,
+              'laserPower': 73,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '017',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 475.76, 'angle': 42.02,
+              'laserPower': 73,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'A2_highFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '018',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 162.17,'angle': 42.02,
+              'laserPower': 27, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '019',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 162.17, 'angle': 42.02,
+              'laserPower': 27,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '020',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 162.17, 'angle': 42.02,
+              'laserPower': 29,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '021',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 448.29,'angle': 42.02,
+              'laserPower': 78, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '022',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 448.29, 'angle': 42.02,
+              'laserPower': 79,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '023',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 448.29, 'angle': 42.02,
+              'laserPower': 79,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'AAF_lowFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '024',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 170.44,'angle': 42.02,
+              'laserPower': 29, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '025',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 170.44, 'angle': 42.02,
+              'laserPower': 29,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '026',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 170.44, 'angle': 42.02,
+              'laserPower': 29,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '027',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 430.89,'angle': 42.02,
+              'laserPower': 73, 'wavelength': 920, 'nFrames': 5500,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'AMfading',
+              'pmt': [0,1], 'paradigm':'sound_tuning'}
+sessions.append(newSession)
+#320 trials
+
+newSession = {'subject':subject, 'date':'20260730','session': '028',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 430.89, 'angle': 42.02,
+              'laserPower': 73,'wavelength': 920, 'nFrames': 6115,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningFreq',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+newSession = {'subject':subject, 'date':'20260730','session': '029',
+              'fps': 9.96, 'magnification': 2.0, 'depth': 430.89, 'angle': 42.02,
+              'laserPower': 73,'wavelength': 920, 'nFrames': 4263,
+              'brainArea': 'AAF_highFreq', 'sessionLabel': 'tuningAM',
+              'pmt': [0,1], 'paradigm':'am_tuning'}
+sessions.append(newSession)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
